b961ed377bc81a87fddc1064baad49335a874657
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2022-04-06 10:08:54 +0200

    Serviceuser og schema registry for on-prem (#11)
    * Lagt til syfosoknad sin serviceuser for å kunne lese i fra on-prem kafka
    
    * On-prem kafka config
    
    * Schema registry

--- src/main/kotlin/no/nav/syfo/Application.kt
+++ src/main/kotlin/no/nav/syfo/Application.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableJwtTokenValidation
 @EnableRetry
 @EnableScheduling
+@EnableKafka
 class Application
 
 fun main(args: Array<String>) {
