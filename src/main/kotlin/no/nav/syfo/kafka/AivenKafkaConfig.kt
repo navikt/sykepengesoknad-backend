d6ff80b5a71d834648e74ea38a7fef7b8993ba3c
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-04-12 14:33:31 +0200

    Prøver igjen i dev

--- src/main/kotlin/no/nav/syfo/kafka/AivenKafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/kafka/AivenKafkaConfig.kt
@@ -111,7 +111,7 @@ class AivenKafkaConfig(
         kafkaErrorHandler: AivenKafkaErrorHandler
     ): ConcurrentKafkaListenerContainerFactory<String, String> {
         val config = mapOf(
-            ConsumerConfig.GROUP_ID_CONFIG to "migrering-import-consumer",
+            ConsumerConfig.GROUP_ID_CONFIG to "migrering-import-consumer-2",
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
             ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
