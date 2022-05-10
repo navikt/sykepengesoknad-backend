5493015af16b186d5962d4b3a90e189e5b5ff316
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-10 12:31:01 +0200

    Fjerner gammelt dine sykmeldte API

--- src/main/kotlin/no/nav/syfo/controller/domain/soknadarbeidsgiver/RSSoknadArbeidsgiverRespons.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.syfo.controller.domain.soknadarbeidsgiver
-
-data class RSSoknadArbeidsgiverRespons(
-    val narmesteLedere: List<RSSoknadrelasjon>,
-    val humanResources: List<RSSoknadrelasjon>
-)
