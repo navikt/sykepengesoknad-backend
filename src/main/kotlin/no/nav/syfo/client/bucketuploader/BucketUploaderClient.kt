3ba8dc99668f19027a0ca99a730aa268b2c8fd1c
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-05-18 21:35:15 +0200

    Håndter exception fra bucket-klient

--- src/main/kotlin/no/nav/syfo/client/bucketuploader/BucketUploaderClient.kt
+++ src/main/kotlin/no/nav/syfo/client/bucketuploader/BucketUploaderClient.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.client.bucketuploader
 
+import no.nav.syfo.logger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
@@ -11,12 +12,19 @@ class BucketUploaderClient(
     private val flexBucketUploaderRestTemplate: RestTemplate,
 ) {
 
+    private val log = logger()
+
     fun slettKvittering(blobName: String): Boolean {
-        val response = flexBucketUploaderRestTemplate.getForEntity(
-            "$bucketUploderUrl/maskin/slett/$blobName",
-            VedleggRespons::class.java
-        )
-        return response.statusCode.is2xxSuccessful
+        try {
+            val response = flexBucketUploaderRestTemplate.getForEntity(
+                "$bucketUploderUrl/maskin/slett/$blobName",
+                VedleggRespons::class.java
+            )
+            return response.statusCode.is2xxSuccessful
+        } catch (e: Exception) {
+            log.warn("Kunne ikke slette kvittering med blobId $blobName", e)
+        }
+        return false
     }
 }
 
