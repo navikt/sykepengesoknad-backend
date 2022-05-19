f8f4f2b0bd0b7d4c0f69824d18711cd4d136d677
golshidbahadorian <golshid.bahadorian@nav.no>
2022-05-19 13:15:46 +0200

    Partitionerer basert på Fnr

---
+++ src/main/kotlin/no/nav/syfo/kafka/FnrPartitioner.kt
@@ -0,0 +1,13 @@
+package no.nav.syfo.kafka
+
+import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner
+import org.apache.kafka.common.Cluster
+
+class FnrPartitioner : DefaultPartitioner() {
+    override fun partition(topic: String, key: Any, keyBytes: ByteArray, value: Any, valueBytes: ByteArray, cluster: Cluster, numPartitions: Int): Int {
+        val soknad = value as SykepengesoknadDTO
+        val actualKey: String = soknad.fnr
+        return super.partition(topic, actualKey, actualKey.toByteArray(), value, valueBytes, cluster, numPartitions)
+    }
+}
