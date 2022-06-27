4f997d19ac347b9ed0f8a78aa8f6ff7e0a209e7a
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-06-27 15:32:27 +0200

    Legger organisasjoner på kafka

--- src/main/kotlin/no/nav/helse/flex/cronjob/Orgnavn.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/Orgnavn.kt
@@ -2,6 +2,8 @@ package no.nav.helse.flex.cronjob
 
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.repository.SykepengesoknadRepository
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
@@ -10,6 +12,7 @@ import java.util.concurrent.TimeUnit
 class Orgnavn(
     private val leaderElection: LeaderElection,
     private val repository: SykepengesoknadRepository,
+    private val kafkaProducer: KafkaProducer<String, String>,
 ) {
 
     private val log = logger()
@@ -23,6 +26,17 @@ class Orgnavn(
 
     fun hentSisteOrgnavn() {
         val organisasjoner = repository.findLatestOrgnavn()
+
         log.info(organisasjoner.toString())
+
+        organisasjoner.forEach {
+            kafkaProducer.send(
+                ProducerRecord(
+                    "flex.organisasjoner",
+                    it.first,
+                    it.second
+                )
+            )
+        }
     }
 }
