3a692c6caa64c723cff123928f064d374c1355f6
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-08-18 11:44:32 +0200

    Skill på Header og MDC-marker

--- src/main/kotlin/no/nav/helse/flex/util/LogRequestIdFilter.kt
+++ src/main/kotlin/no/nav/helse/flex/util/LogRequestIdFilter.kt
@@ -7,20 +7,22 @@ import javax.servlet.http.HttpFilter
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-private const val X_REQUEST_ID = "x-request-id"
+private const val X_REQUEST_ID_HEADER = "x-request-id"
+private const val X_REQUEST_ID_MDC_MARKER = "x_request_id"
 
 @Component
 class LogRequestIdFilter : HttpFilter() {
 
     override fun doFilter(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
         try {
-            request.getHeader(X_REQUEST_ID)?.let {
-                MDC.put(X_REQUEST_ID, it)
-                response.setHeader(X_REQUEST_ID, it)
+            request.getHeader(X_REQUEST_ID_HEADER)?.let {
+                MDC.put(X_REQUEST_ID_MDC_MARKER, it)
+                // Legger på x-request-id på response for bedre feilhåndtering i frontend.
+                response.setHeader(X_REQUEST_ID_HEADER, it)
             }
             filterChain.doFilter(request, response)
         } finally {
-            MDC.remove(X_REQUEST_ID)
+            MDC.remove(X_REQUEST_ID_HEADER)
         }
     }
 }
