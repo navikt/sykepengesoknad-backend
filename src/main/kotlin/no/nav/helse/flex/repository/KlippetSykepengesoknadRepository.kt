4e7ab6af90789c87c78c888094646dbcb7ea46f5
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-12-06 15:42:39 +0100

    KlippetSykepengesoknadRepository

---
+++ src/main/kotlin/no/nav/helse/flex/repository/KlippetSykepengesoknadRepository.kt
@@ -0,0 +1,29 @@
+package no.nav.helse.flex.repository
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.time.Instant
+
+@Repository
+interface KlippetSykepengesoknadRepository : CrudRepository<KlippetSykepengesoknadDbRecord, String>
+
+@Table("klippet_sykepengesoknad")
+data class KlippetSykepengesoknadDbRecord(
+    @Id
+    val id: String? = null,
+    val sykepengesoknadUuid: String,
+    val sykmeldingUuid: String,
+    val klippVariant: KlippVariant,
+    val periodeFor: String,
+    val periodeEtter: String?,
+    val timestamp: Instant,
+)
+
+enum class KlippVariant {
+    SOKNAD_STARTER_FOR_SLUTTER_INNI,
+    SOKNAD_STARTER_INNI_SLUTTER_ETTER,
+    SOKNAD_STARTER_FOR_SLUTTER_ETTER,
+    SOKNAD_STARTER_INNI_SLUTTER_INNI,
+}
