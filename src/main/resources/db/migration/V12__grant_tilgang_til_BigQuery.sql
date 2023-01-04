872d54388c3b88bcb4f493a689e12dab96b51188
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-01-04 08:48:23 +0100

    Revert "Endre grant fra select til update"
    This reverts commit fd51d383827b56728f1ccbe36c3c3f2766650d52.

--- src/main/resources/db/migration/V12__grant_tilgang_til_BigQuery.sql
+++ src/main/resources/db/migration/V12__grant_tilgang_til_BigQuery.sql
@@ -4,7 +4,7 @@ $$
         IF EXISTS
             (SELECT 1 FROM pg_user where usename = 'bigquery-dataprodukt')
         THEN
-            GRANT UPDATE ON klippet_sykepengesoknad TO "bigquery-dataprodukt";
+            GRANT SELECT ON klippet_sykepengesoknad TO "bigquery-dataprodukt";
         END IF;
     END
 $$;
