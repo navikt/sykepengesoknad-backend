15bbaf0b6886a5af5b19b225626a49c26ba172da
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-01-04 08:49:23 +0100

    Endre til update i riktig fil

--- src/main/resources/db/migration/V14__grant_update_til_cloudsqliamuser.sql
+++ src/main/resources/db/migration/V14__grant_update_til_cloudsqliamuser.sql
@@ -4,7 +4,7 @@ $$
         IF EXISTS
             (SELECT 1 from pg_roles where rolname = 'cloudsqliamuser')
         THEN
-            GRANT SELECT ON sykepengesoknad TO cloudsqliamuser;
+            GRANT UPDATE ON sykepengesoknad TO cloudsqliamuser;
         END IF;
     END
 $$;
