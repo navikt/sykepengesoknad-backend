ee2d159f63502370139f71b75683d53a5970e1ca
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-01-04 13:38:34 +0100

    Ikke lagre til tabell som skal endre seg (#191)

--- src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/KlippMetrikk.kt
+++ src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/KlippMetrikk.kt
@@ -1,23 +1,12 @@
 package no.nav.helse.flex.soknadsopprettelse
 
-import no.nav.helse.flex.repository.KlippMetrikkDbRecord
 import no.nav.helse.flex.repository.KlippMetrikkRepository
 import org.springframework.stereotype.Component
-import java.time.Instant
 
 @Component
 class KlippMetrikk(private val klippMetrikkRepository: KlippMetrikkRepository) {
 
     fun klippMetrikk(klippMetrikkVariant: KlippMetrikkVariant, soknadstatus: String, sykmeldingId: String) {
-        klippMetrikkRepository.save(
-            KlippMetrikkDbRecord(
-                id = null,
-                sykmeldingUuid = sykmeldingId,
-                variant = klippMetrikkVariant.name,
-                soknadstatus = soknadstatus,
-                timestamp = Instant.now()
-            )
-        )
     }
 
     enum class KlippMetrikkVariant {
