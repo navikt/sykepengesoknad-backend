b1106d8d23868a0c71f7a89e180099c0f9917551
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2023-01-09 15:05:55 +0100

    Fortsetter aktivering av andre søknader når det kastes feil (#194)

--- src/main/kotlin/no/nav/helse/flex/aktivering/kafka/AktiveringConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/aktivering/kafka/AktiveringConsumer.kt
@@ -2,6 +2,7 @@ package no.nav.helse.flex.aktivering.kafka
 
 import no.nav.helse.flex.aktivering.AktiverEnkeltSoknad
 import no.nav.helse.flex.kafka.sykepengesoknadAktiveringTopic
+import no.nav.helse.flex.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -9,8 +10,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class AktiveringConsumer(
-    private val aktiverEnkeltSoknad: AktiverEnkeltSoknad
+    private val aktiverEnkeltSoknad: AktiverEnkeltSoknad,
 ) {
+    private val log = logger()
 
     @KafkaListener(
         topics = [sykepengesoknadAktiveringTopic],
@@ -21,8 +23,13 @@ class AktiveringConsumer(
         concurrency = "4"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        try {
+            aktiverEnkeltSoknad.aktiverSoknad(cr.key())
+        } catch (e: Exception) {
+            // De som feiler blir lagt tilbake igjen av AktiveringJob
+            log.warn("Feilet ved aktivering av søknad ${cr.key()}, forsøker igjen senere", e)
+        }
 
-        aktiverEnkeltSoknad.aktiverSoknad(cr.key())
         acknowledgment.acknowledge()
     }
 }
