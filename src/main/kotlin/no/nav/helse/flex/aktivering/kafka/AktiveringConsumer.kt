4734b1aaeb669bc22c73767c9f151803cad3a476
kulsveen <bjorn.inge.kulsveen@nav.no>
2023-01-09 13:49:46 +0100

    Legger aktivering tilbake på topic når den feiler

--- src/main/kotlin/no/nav/helse/flex/aktivering/kafka/AktiveringConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/aktivering/kafka/AktiveringConsumer.kt
@@ -1,7 +1,10 @@
 package no.nav.helse.flex.aktivering.kafka
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.aktivering.AktiverEnkeltSoknad
 import no.nav.helse.flex.kafka.sykepengesoknadAktiveringTopic
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.util.OBJECT_MAPPER
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
@@ -9,8 +12,10 @@ import org.springframework.stereotype.Component
 
 @Component
 class AktiveringConsumer(
-    private val aktiverEnkeltSoknad: AktiverEnkeltSoknad
+    private val aktiverEnkeltSoknad: AktiverEnkeltSoknad,
+    private val aktiveringProducer: AktiveringProducer,
 ) {
+    private val log = logger()
 
     @KafkaListener(
         topics = [sykepengesoknadAktiveringTopic],
@@ -21,8 +26,15 @@ class AktiveringConsumer(
         concurrency = "4"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        try {
+            aktiverEnkeltSoknad.aktiverSoknad(cr.key())
+        } catch (e: Exception) {
+            // Forsøker å aktivere denne senere
+            log.warn("Feilet ved aktivering av søknad ${cr.key()}, legger den tilbake og forsøker igjen senere")
+            val aktiveringBestilling: AktiveringBestilling = OBJECT_MAPPER.readValue(cr.value())
+            aktiveringProducer.leggPaAktiveringTopic(aktiveringBestilling)
+        }
 
-        aktiverEnkeltSoknad.aktiverSoknad(cr.key())
         acknowledgment.acknowledge()
     }
 }
