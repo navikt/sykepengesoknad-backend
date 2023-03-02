c4f8c92b0ddff41636e0b91f85c9abad8028cadc
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-03-02 07:28:44 +0100

    Reenable job

--- src/main/kotlin/no/nav/helse/flex/personhendelse/AutomatiskInnsendingJob.kt
+++ src/main/kotlin/no/nav/helse/flex/personhendelse/AutomatiskInnsendingJob.kt
@@ -3,9 +3,11 @@ package no.nav.helse.flex.personhendelse
 import no.nav.helse.flex.config.EnvironmentToggles
 import no.nav.helse.flex.cronjob.LeaderElection
 import no.nav.helse.flex.util.osloZone
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.util.concurrent.TimeUnit
 
 @Component
 class AutomatiskInnsendingJob(
@@ -14,7 +16,7 @@ class AutomatiskInnsendingJob(
     val env: EnvironmentToggles,
 ) {
 
-    // @Scheduled(initialDelay = 5, fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(initialDelay = 5, fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
     fun automatiskUtsending() {
         if (leaderElection.isLeader()) {
             if (env.isNotProduction() || LocalDate.now(osloZone).dayOfWeek == DayOfWeek.WEDNESDAY) {
