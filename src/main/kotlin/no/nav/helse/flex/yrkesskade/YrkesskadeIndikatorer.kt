58a1ba548ce625ebbf254ce5ea8f9107b89a1e7a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-03-20 13:06:47 +0100

    Enable yrkesskade spørsmål (#239)

--- src/main/kotlin/no/nav/helse/flex/yrkesskade/YrkesskadeIndikatorer.kt
+++ src/main/kotlin/no/nav/helse/flex/yrkesskade/YrkesskadeIndikatorer.kt
@@ -4,23 +4,16 @@ import no.nav.helse.flex.client.yrkesskade.HarYsSak
 import no.nav.helse.flex.client.yrkesskade.HarYsSakerRequest
 import no.nav.helse.flex.client.yrkesskade.YrkesskadeClient
 import no.nav.helse.flex.service.FolkeregisterIdenter
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 class YrkesskadeIndikatorer(
 
-    @Value("\${YRKESSKADE_ENABLET:false}")
-    private val yrkesskadeEnablet: Boolean,
     private val yrkesskadeClient: YrkesskadeClient,
     private val yrkesskadeSykmeldingRepository: YrkesskadeSykmeldingRepository
 ) {
 
     fun harYrkesskadeIndikatorer(identer: FolkeregisterIdenter, sykmeldingId: String?): Boolean {
-        if (!yrkesskadeEnablet) {
-            return false
-        }
-
         sykmeldingId?.let {
             if (yrkesskadeSykmeldingRepository.existsBySykmeldingId(sykmeldingId)) {
                 return true
