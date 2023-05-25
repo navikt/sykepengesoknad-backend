8e3a886db2d8a5ddbd936896573a92262e5916aa
Øivind <oivind.thorsen.kulsrud@nav.no>
2023-05-25 09:37:48 +0200

    slutter å kalle frilanser/selvstendig næringsdrivende en job

--- src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/SelvstendigeOgFrilansere.kt
+++ src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/SelvstendigeOgFrilansere.kt
@@ -89,7 +89,7 @@ fun jobbetDuGradert(
 ): Sporsmal {
     return Sporsmal(
         tag = JOBBET_DU_GRADERT + index,
-        sporsmalstekst = "Sykmeldingen sier du kunne jobbe ${100 - periode.grad} % i jobben din som $arbeidssituasjon. Jobbet du mer enn det?",
+        sporsmalstekst = "Sykmeldingen sier du kunne jobbe ${100 - periode.grad} % som $arbeidssituasjon. Jobbet du mer enn det?",
         svartype = Svartype.JA_NEI,
         kriterieForVisningAvUndersporsmal = Visningskriterie.JA,
         undersporsmal = jobbetDuGradertUndersporsmal(periode, 100 + 1 - periode.grad, index)
