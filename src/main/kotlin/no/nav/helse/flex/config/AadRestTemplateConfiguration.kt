ddea9ab797983ed04395c53011f3e381e392d05b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-06-14 12:33:51 +0200

    Implementer RestTemplate timeout test

--- src/main/kotlin/no/nav/helse/flex/config/AadRestTemplateConfiguration.kt
+++ src/main/kotlin/no/nav/helse/flex/config/AadRestTemplateConfiguration.kt
@@ -5,20 +5,69 @@ import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager
+import org.apache.hc.core5.http.io.SocketConfig
+import org.apache.hc.core5.util.Timeout
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpRequest
 import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 import java.time.Duration
+import java.util.function.Supplier
+
+const val MEDLEMSKAP_VURDERING_REST_TEMPLATE_CONNECT_TIMEOUT = 2L
+const val MEDLEMSKAP_VURDERING_REST_TEMPLATE_READ_TIMEOUT = 25L
 
 @EnableOAuth2Client(cacheEnabled = true)
 @Configuration
 class AadRestTemplateConfiguration {
 
     val log = logger()
+
+    @Bean
+    fun httpClient(): CloseableHttpClient {
+        val connectionManager = PoolingHttpClientConnectionManager()
+        connectionManager.defaultMaxPerRoute = 50
+        connectionManager.maxTotal = 50
+        // Erstatter deprecated HttpComponentsClientHttpRequestFactory.setReadTimeout()
+        connectionManager.defaultSocketConfig = SocketConfig.custom()
+            .setSoTimeout(
+                Timeout.of(
+                    MEDLEMSKAP_VURDERING_REST_TEMPLATE_READ_TIMEOUT,
+                    java.util.concurrent.TimeUnit.SECONDS
+                )
+            )
+            .build()
+
+        return HttpClientBuilder.create()
+            .setConnectionManager(connectionManager)
+            .build()
+    }
+
+    @Bean
+    fun medlemskapVurderingRestTemplate(
+        restTemplateBuilder: RestTemplateBuilder,
+        httpClient: CloseableHttpClient,
+        clientConfigurationProperties: ClientConfigurationProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService
+    ): RestTemplate {
+        val registrationName = "medlemskap-vurdering-sykepenger-client-credentials"
+        val clientProperties = clientConfigurationProperties.registration[registrationName]
+            ?: throw RuntimeException("Fant ikke config for $registrationName.")
+
+        return restTemplateBuilder
+            // https://kotlinlang.org/docs/fun-interfaces.html#sam-conversions
+            .requestFactory(Supplier { HttpComponentsClientHttpRequestFactory(httpClient) })
+            .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
+            .setConnectTimeout(Duration.ofSeconds(MEDLEMSKAP_VURDERING_REST_TEMPLATE_CONNECT_TIMEOUT))
+            .build()
+    }
 
     @Bean
     fun sykepengesoknadKvitteringerRestTemplate(
@@ -45,24 +94,6 @@ class AadRestTemplateConfiguration {
             clientConfigurationProperties = clientConfigurationProperties,
             oAuth2AccessTokenService = oAuth2AccessTokenService
         )
-
-    @Bean
-    fun medlemskapVurderingRestTemplate(
-        restTemplateBuilder: RestTemplateBuilder,
-        clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
-    ): RestTemplate {
-        val registrationName = "medlemskap-vurdering-sykepenger-client-credentials"
-        val clientProperties =
-            clientConfigurationProperties.registration[registrationName]
-                ?: throw RuntimeException("Fant ikke config for $registrationName.")
-        return restTemplateBuilder
-            .setConnectTimeout(Duration.ofSeconds(2L))
-            // LovMe har intern timeout på 20 sekunder på sine kall, og trenger å gjøre en beregning før den returnerer.
-            .setReadTimeout(Duration.ofSeconds(25L))
-            .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
-            .build()
-    }
 
     @Bean
     fun syfotilgangskontrollRestTemplate(
