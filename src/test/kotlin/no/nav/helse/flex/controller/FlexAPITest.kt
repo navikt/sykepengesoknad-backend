f93e5a599a46aaf5fea7e76375cb8ea963d9a0c7
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-06-20 11:10:18 +0200

    Identapi for intern frontend (#309)

--- src/test/kotlin/no/nav/helse/flex/controller/FlexAPITest.kt
+++ src/test/kotlin/no/nav/helse/flex/controller/FlexAPITest.kt
@@ -2,6 +2,7 @@ package no.nav.helse.flex.controller
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.BaseTestClass
+import no.nav.helse.flex.client.pdl.PdlIdent
 import no.nav.helse.flex.jwt
 import no.nav.helse.flex.sendSykmelding
 import no.nav.helse.flex.skapAzureJwt
@@ -71,6 +72,38 @@ class FlexAPITest : BaseTestClass() {
                     .get("/api/v1/flex/sykepengesoknader")
                     .header("Authorization", "Bearer ${jwt(fnr)}")
                     .header("fnr", fnr)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(MockMvcResultMatchers.status().is4xxClientError)
+    }
+
+    @Test
+    fun `Kan hente identer fra flex-internal-frontend`() {
+        val result = mockMvc
+            .perform(
+                MockMvcRequestBuilders
+                    .get("/api/v1/flex/identer")
+                    .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id")}")
+                    .header("ident", "211111111111")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(MockMvcResultMatchers.status().isOk).andReturn()
+
+        val fraRest: List<PdlIdent> = OBJECT_MAPPER.readValue(result.response.contentAsString)
+        fraRest.shouldHaveSize(3)
+        fraRest[0] shouldBeEqualTo PdlIdent("FOLKEREGISTERIDENT", "211111111111")
+        fraRest[1] shouldBeEqualTo PdlIdent("FOLKEREGISTERIDENT", "111111111111")
+        fraRest[2] shouldBeEqualTo PdlIdent("AKTORID", "21111111111100")
+    }
+
+    @Test
+    fun `Kan ikke hente identer som bruker`() {
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders
+                    .get("/api/v1/flex/identer")
+                    .header("Authorization", "Bearer ${jwt(fnr)}")
+                    .header("ident", "211111111111")
                     .contentType(MediaType.APPLICATION_JSON)
             )
             .andExpect(MockMvcResultMatchers.status().is4xxClientError)
