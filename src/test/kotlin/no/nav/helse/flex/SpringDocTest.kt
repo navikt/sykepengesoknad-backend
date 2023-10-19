cccfb72850a05d59035a2beddf5e88808c91653a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-10-19 09:35:43 +0200

    Eksponer openapi spec (#425)

---
+++ src/test/kotlin/no/nav/helse/flex/SpringDocTest.kt
@@ -0,0 +1,19 @@
+package no.nav.helse.flex
+
+import org.amshove.kluent.shouldContain
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+class SpringDocTest : BaseTestClass() {
+
+    @Test
+    fun `har springdoc`() {
+        val response = mockMvc
+            .perform(MockMvcRequestBuilders.get("/v3/api-docs"))
+            .andExpect(MockMvcResultMatchers.status().isOk).andReturn().response.contentAsString
+
+        response.shouldContain("OpenAPI definition")
+        response.shouldContain("oppdaterSporsmal")
+    }
+}
