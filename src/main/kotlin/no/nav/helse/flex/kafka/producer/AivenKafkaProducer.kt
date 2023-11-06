2565549780dce8f9d20ab26bb27e7d84bce99a75
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2023-11-06 14:40:02 +0100

    Debug publiserte søknader

--- src/main/kotlin/no/nav/helse/flex/kafka/producer/AivenKafkaProducer.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/producer/AivenKafkaProducer.kt
@@ -1,8 +1,10 @@
 package no.nav.helse.flex.kafka.producer
 
+import no.nav.helse.flex.config.EnvironmentToggles
 import no.nav.helse.flex.kafka.sykepengesoknadTopic
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
+import no.nav.helse.flex.util.serialisertTilString
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
@@ -10,12 +12,16 @@ import org.springframework.stereotype.Component
 
 @Component
 class AivenKafkaProducer(
-    private val producer: KafkaProducer<String, SykepengesoknadDTO>
+    private val producer: KafkaProducer<String, SykepengesoknadDTO>,
+    private val environmentToggles: EnvironmentToggles
 ) {
     val log = logger()
 
     fun produserMelding(soknad: SykepengesoknadDTO): RecordMetadata {
         try {
+            if (environmentToggles.isQ()) {
+                log.info("Publiserer søknad ${soknad.id} på topic $sykepengesoknadTopic\n${soknad.serialisertTilString()}")
+            }
             return producer.send(
                 ProducerRecord(
                     sykepengesoknadTopic,
