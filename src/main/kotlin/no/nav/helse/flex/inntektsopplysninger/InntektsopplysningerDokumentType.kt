b92b5ff2a2cf8975a09cb041ee984ecf3712fb4b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-12-21 13:04:00 +0100

    Refakturer og test inntektsopplysninger

--- src/main/kotlin/no/nav/helse/flex/inntektsopplysninger/InntektsopplysningerDokumentType.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektsopplysninger/InntektsopplysningerDokumentType.kt
@@ -1,11 +1,15 @@
 package no.nav.helse.flex.inntektsopplysninger
 
+import no.nav.helse.flex.domain.Sykepengesoknad
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.NARINGSSPESIFIKASJON
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.NARINGSSPESIFIKASJON_OPTIONAL
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.REGNSKAP_FORELOPIG
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.REGNSKAP_FORRIGE_AAR
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.SKATTEMELDING
 import no.nav.helse.flex.inntektsopplysninger.InntektsopplysningerDokumentType.SKATTEMELDING_OPTIONAL
+import no.nav.helse.flex.soknadsopprettelse.INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_JA
+import no.nav.helse.flex.soknadsopprettelse.INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_NEI
+import no.nav.helse.flex.soknadsopprettelse.INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT
 import java.time.LocalDate
 
 enum class InntektsopplysningerDokumentType {
@@ -17,7 +21,7 @@ enum class InntektsopplysningerDokumentType {
     REGNSKAP_FORELOPIG
 }
 
-fun dokumenterSomSkalSendes(dagensDato: LocalDate): List<InntektsopplysningerDokumentType> {
+fun dokumenterSomSkalSendesInn(dagensDato: LocalDate): List<InntektsopplysningerDokumentType> {
     val sisteTertialStart = LocalDate.of(dagensDato.year, 9, 1)
     val fristSkattemelding = LocalDate.of(dagensDato.year, 5, 31)
 
@@ -35,4 +39,23 @@ fun dokumenterSomSkalSendes(dagensDato: LocalDate): List<InntektsopplysningerDok
 
         else -> listOf(SKATTEMELDING, NARINGSSPESIFIKASJON, REGNSKAP_FORELOPIG)
     }
+}
+
+fun Sykepengesoknad.inntektsopplysningerMaaDokumenteres(): Boolean {
+    getSporsmalMedTagOrNull(INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_JA)?.let {
+        if (it.forsteSvar == "CHECKED") {
+            return true
+        }
+    }
+
+    getSporsmalMedTagOrNull(INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_NEI)?.let { sporsmal1 ->
+        if (sporsmal1.forsteSvar == "CHECKED") {
+            getSporsmalMedTagOrNull(INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT)?.let {
+                if (it.forsteSvar == "JA") {
+                    return true
+                }
+            }
+        }
+    }
+    return false
 }
