8d89f1bb5c29ec4fd8d0a26472c10e5c05912d36
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-03 15:30:04 +0100

    Bedre tester for api kallet (#516)

--- src/test/kotlin/no/nav/helse/flex/mockdispatcher/InnsendingApiMockDispatcher.kt
+++ src/test/kotlin/no/nav/helse/flex/mockdispatcher/InnsendingApiMockDispatcher.kt
@@ -1,6 +1,9 @@
 package no.nav.helse.flex.mockdispatcher
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.client.innsendingapi.EttersendingRequest
 import no.nav.helse.flex.client.innsendingapi.EttersendingResponse
+import no.nav.helse.flex.util.OBJECT_MAPPER
 import no.nav.helse.flex.util.serialisertTilString
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.QueueDispatcher
@@ -9,7 +12,11 @@ import org.springframework.http.MediaType
 import java.util.UUID
 
 object InnsendingApiMockDispatcher : QueueDispatcher() {
+    private val requests = mutableListOf<RecordedRequest>()
+
     override fun dispatch(request: RecordedRequest): MockResponse {
+        requests.add(request)
+
         fun withContentTypeApplicationJson(createMockResponse: () -> MockResponse): MockResponse =
             createMockResponse().addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
 
@@ -24,5 +31,11 @@ object InnsendingApiMockDispatcher : QueueDispatcher() {
                 ).serialisertTilString(),
             )
         }
+    }
+
+    fun getRequests(): List<RecordedRequest> = requests.toList()
+
+    fun lastRequest(): EttersendingRequest {
+        return OBJECT_MAPPER.readValue(requests.last().body.readUtf8())
     }
 }
