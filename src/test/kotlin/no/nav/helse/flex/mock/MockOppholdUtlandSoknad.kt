0bd0b42f58cc76cbaa509452f0a3944c460059a8
golshidbahadorian <golshid.bahadorian@nav.no>
2024-01-12 10:36:53 +0100

    Fikser test

--- src/test/kotlin/no/nav/helse/flex/mock/MockOppholdUtlandSoknad.kt
+++ src/test/kotlin/no/nav/helse/flex/mock/MockOppholdUtlandSoknad.kt
@@ -1,13 +1,7 @@
 package no.nav.helse.flex.mock
 
 import no.nav.helse.flex.domain.Sykepengesoknad
-import no.nav.helse.flex.soknadsopprettelse.ARBEIDSGIVER
-import no.nav.helse.flex.soknadsopprettelse.BEKREFT_OPPLYSNINGER_UTLAND
-import no.nav.helse.flex.soknadsopprettelse.FERIE
-import no.nav.helse.flex.soknadsopprettelse.LAND
-import no.nav.helse.flex.soknadsopprettelse.PERIODEUTLAND
-import no.nav.helse.flex.soknadsopprettelse.SYKMELDINGSGRAD
-import no.nav.helse.flex.soknadsopprettelse.settOppSoknadOppholdUtland
+import no.nav.helse.flex.soknadsopprettelse.*
 import no.nav.helse.flex.testutil.besvarsporsmal
 import no.nav.helse.flex.util.DatoUtil.periodeTilJson
 import java.time.LocalDate.now
@@ -24,7 +18,8 @@ private fun leggSvarPaSoknad(
         .perioder()
         .arbeidsgiver(feriesvar)
         .besvarsporsmal(LAND, "England")
-        .besvarsporsmal(BEKREFT_OPPLYSNINGER_UTLAND, "CHECKED")
+        .besvarsporsmal(TIL_SLUTT, "Jeg lover å ikke lyve!")
+        .besvarsporsmal(BEKREFT_OPPLYSNINGER,"CHECKED")
 }
 
 private fun Sykepengesoknad.arbeidsgiver(feriesvarverdi: String): Sykepengesoknad {
