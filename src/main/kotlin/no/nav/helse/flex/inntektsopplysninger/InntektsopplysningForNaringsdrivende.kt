4613a3963ba433074c6e35a4dcba7b3edd0955c1
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-24 15:39:22 +0100

    Korrigering håndtering

--- src/main/kotlin/no/nav/helse/flex/inntektsopplysninger/InntektsopplysningForNaringsdrivende.kt
+++ src/main/kotlin/no/nav/helse/flex/inntektsopplysninger/InntektsopplysningForNaringsdrivende.kt
@@ -6,6 +6,7 @@ import no.nav.helse.flex.client.innsendingapi.Vedlegg
 import no.nav.helse.flex.domain.Arbeidssituasjon
 import no.nav.helse.flex.domain.Soknadstype
 import no.nav.helse.flex.domain.Sykepengesoknad
+import no.nav.helse.flex.repository.SykepengesoknadDbRecord
 import no.nav.helse.flex.repository.SykepengesoknadRepository
 import no.nav.helse.flex.soknadsopprettelse.INNTEKTSOPPLYSNINGER_DRIFT_VIRKSOMHETEN
 import org.springframework.stereotype.Service
@@ -38,38 +39,53 @@ class InntektsopplysningForNaringsdrivende(
 
         val visNyKvittering = soknad.getSporsmalMedTagOrNull(INNTEKTSOPPLYSNINGER_DRIFT_VIRKSOMHETEN) != null
 
-        sykepengesoknadRepository.findBySykepengesoknadUuid(soknad.id)?.let { sykepengeSoknad ->
-            val oppdatertSoknad =
-                if (soknad.inntektsopplysningerMaaDokumenteres()) {
-                    val dokumenter = dokumenterSomSkalSendesInn(LocalDate.now())
+        val sykepengesoknadDbRecord =
+            sykepengesoknadRepository.findBySykepengesoknadUuid(soknad.id)
+                ?: throw IllegalStateException("Fant ikke søknad")
 
-                    val innsendingResponse =
-                        innsendingApiClient.opprettEttersending(
-                            EttersendingRequest(
-                                skjemanr = "NAV 08-35.01",
-                                sprak = "nb_NO",
-                                tema = "SYK",
-                                vedleggsListe =
-                                    dokumenter.map {
-                                        Vedlegg(it.vedleggsnr, it.tittel)
-                                    },
-                                brukernotifikasjonstype = "oppgave",
-                                koblesTilEksisterendeSoknad = false,
-                            ),
+        fun skapOppdatertSoknad(): SykepengesoknadDbRecord {
+            if (soknad.inntektsopplysningerMaaDokumenteres()) {
+                if (soknad.korrigerer != null) {
+                    val korrigertSoknad =
+                        sykepengesoknadRepository.findBySykepengesoknadUuid(soknad.korrigerer)
+                    if (korrigertSoknad?.inntektsopplysningerInnsendingId != null) {
+                        return sykepengesoknadDbRecord.copy(
+                            inntektsopplysningerNyKvittering = korrigertSoknad.inntektsopplysningerNyKvittering,
+                            inntektsopplysningerInnsendingId = korrigertSoknad.inntektsopplysningerInnsendingId,
+                            inntektsopplysningerInnsendingDokumenter = korrigertSoknad.inntektsopplysningerInnsendingDokumenter,
                         )
-
-                    sykepengeSoknad.copy(
-                        inntektsopplysningerNyKvittering = visNyKvittering,
-                        inntektsopplysningerInnsendingId = innsendingResponse.innsendingsId,
-                        inntektsopplysningerInnsendingDokumenter = dokumenter.joinToString(","),
-                    )
-                } else {
-                    sykepengeSoknad.copy(
-                        inntektsopplysningerNyKvittering = visNyKvittering,
-                    )
+                    }
                 }
 
-            sykepengesoknadRepository.save(oppdatertSoknad)
+                val dokumenter = dokumenterSomSkalSendesInn(LocalDate.now())
+
+                val innsendingResponse =
+                    innsendingApiClient.opprettEttersending(
+                        EttersendingRequest(
+                            skjemanr = "NAV 08-35.01",
+                            sprak = "nb_NO",
+                            tema = "SYK",
+                            vedleggsListe =
+                                dokumenter.map {
+                                    Vedlegg(it.vedleggsnr, it.tittel)
+                                },
+                            brukernotifikasjonstype = "oppgave",
+                            koblesTilEksisterendeSoknad = false,
+                        ),
+                    )
+
+                return sykepengesoknadDbRecord.copy(
+                    inntektsopplysningerNyKvittering = visNyKvittering,
+                    inntektsopplysningerInnsendingId = innsendingResponse.innsendingsId,
+                    inntektsopplysningerInnsendingDokumenter = dokumenter.joinToString(","),
+                )
+            } else {
+                return sykepengesoknadDbRecord.copy(
+                    inntektsopplysningerNyKvittering = visNyKvittering,
+                )
+            }
         }
+
+        sykepengesoknadRepository.save(skapOppdatertSoknad())
     }
 }
