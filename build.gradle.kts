3e1841761511c61e4cf23b0b657285c16c0c80a1
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-01-25 07:55:30 +0100

    Source version 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 group = "no.nav.helse.flex"
 version = "1"
 description = "sykepengesoknad-backend"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 ext["okhttp3.version"] = "4.9.3" // Token-support tester trenger Mockwebserver.
 
@@ -92,7 +92,7 @@ tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar
 
 tasks.withType<KotlinCompile> {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
+        jvmTarget.set(JvmTarget.JVM_21)
         freeCompilerArgs.add("-Xjsr305=strict")
 
         if (System.getenv("CI") == "true") {
