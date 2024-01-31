63fc69472723ef0fe95dd4758b5b15cea9bc3277
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-01-31 06:47:42 +0100

    Log 2xx requests og test filterrekkefølge

--- src/main/kotlin/no/nav/helse/flex/controller/ClientErrorLogFilter.kt
+++ src/main/kotlin/no/nav/helse/flex/controller/ClientErrorLogFilter.kt
@@ -2,12 +2,12 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.helse.flex.logger
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import org.springframework.web.util.ContentCachingRequestWrapper
-import org.springframework.web.util.ContentCachingResponseWrapper
 
 @Component
+@Order(1)
 class ClientErrorLogFilter : OncePerRequestFilter() {
     private val log = logger()
 
@@ -16,14 +16,12 @@ class ClientErrorLogFilter : OncePerRequestFilter() {
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        val requestWrapper = ContentCachingRequestWrapper(request)
-        val responseWrapper = ContentCachingResponseWrapper(response)
+        filterChain.doFilter(request, response)
 
-        filterChain.doFilter(requestWrapper, responseWrapper)
-        val is4xx = responseWrapper.status.toString().startsWith("4")
+        val is4xx = response.status.toString().startsWith("4")
+        val is2xx = response.status.toString().startsWith("2")
 
-        if (is4xx) {
-            log.warn("HTTP 4xx-feilrespons: ${responseWrapper.status} - ${requestWrapper.requestURI}")
-        }
+        if (is2xx) log.info("HTTP 2xx-feilrespons: ${response.status} - ${request.requestURI}")
+        if (is4xx) log.warn("HTTP 4xx-feilrespons: ${response.status} - ${request.requestURI}")
     }
 }
--- src/main/kotlin/no/nav/helse/flex/util/LogRequestIdFilter.kt
+++ src/main/kotlin/no/nav/helse/flex/util/LogRequestIdFilter.kt
@@ -4,14 +4,19 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpFilter
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import no.nav.helse.flex.logger
 import org.slf4j.MDC
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 
 private const val X_REQUEST_ID_HEADER = "x-request-id"
 private const val X_REQUEST_ID_MDC_MARKER = "x_request_id"
 
 @Component
+@Order(2)
 class LogRequestIdFilter : HttpFilter() {
+    private val log = logger()
+
     override fun doFilter(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -21,6 +26,7 @@ class LogRequestIdFilter : HttpFilter() {
             request.getHeader(X_REQUEST_ID_HEADER)?.let {
                 MDC.put(X_REQUEST_ID_MDC_MARKER, it)
                 // Legger på x-request-id på response for bedre feilhåndtering i frontend.
+                log.info("Legger på $X_REQUEST_ID_HEADER på response.")
                 response.setHeader(X_REQUEST_ID_HEADER, it)
             }
             filterChain.doFilter(request, response)
