22c3c5f043ea3046e387796e1372f43b7c87b4f1
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-02-22 12:07:42 +0100

    Bruk database til å holde oversikt over retries

--- src/main/kotlin/no/nav/helse/flex/aktivering/RetryLogger.kt
+++ src/main/kotlin/no/nav/helse/flex/aktivering/RetryLogger.kt
@@ -7,22 +7,25 @@ import org.springframework.stereotype.Service
 const val MAX_RETRIES_BEFORE_ERROR = 5
 
 @Service
-class RetryLogger {
+class RetryLogger(
+    private val retryRepository: RetryRepository,
+) {
     private final val log = logger()
 
-    // TODO: Bytt ut med databasebackend.
-    private val retries = mutableMapOf<String, Int>()
-
     fun inkrementerRetriesOgReturnerLogger(id: String): LoggerFunction {
-        retries[id] = retries.getOrDefault(id, 0) + 1
-        val antallRetries = retries[id] ?: 0
-
-        // Det ville sett mye bedre å returnerer en lambda av typen (String, Throwable) -> Unit
-        // men vi trenger klasser for å kunne asserte på noe i testene.
-        return if (antallRetries > MAX_RETRIES_BEFORE_ERROR) {
-            ErrorLogger(log)
-        } else {
-            WarnLogger(log)
+        try {
+            val retries = retryRepository.inkrementerRetries(id)
+            // Det ville sett mye bedre å returnerer en lambda av typen (String, Throwable) -> Unit
+            // men vi trenger klasser for å kunne asserte type i testene.
+            return if (retries.retryCount > MAX_RETRIES_BEFORE_ERROR) {
+                ErrorLogger(log)
+            } else {
+                WarnLogger(log)
+            }
+        } catch (e: Exception) {
+            // Sikrer at vi får returnert en logger selv om noe feiler ved inkrementering.
+            log.error("Feil ved inkrementering av retries for sykepengesoknad $id.", e)
+            return ErrorLogger(log)
         }
     }
 }
--- src/test/kotlin/no/nav/helse/flex/aktivering/RetryLoggerTest.kt
+++ src/test/kotlin/no/nav/helse/flex/aktivering/RetryLoggerTest.kt
@@ -1,10 +1,22 @@
 package no.nav.helse.flex.aktivering
 
+import no.nav.helse.flex.FellesTestOppsett
 import org.amshove.kluent.`should be instance of`
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
-class RetryLoggerTest {
-    private var retryLogger = RetryLogger()
+class RetryLoggerTest : FellesTestOppsett() {
+    @Autowired
+    private lateinit var retryLogger: RetryLogger
+
+    @Autowired
+    private lateinit var retryRepository: RetryRepository
+
+    @BeforeAll
+    fun setUp() {
+        retryRepository.deleteAll()
+    }
 
     @Test
     fun `ErrorLogger skal returneres når antall kall overstiger MAX_WARNINGS med en gitt ID`() {
