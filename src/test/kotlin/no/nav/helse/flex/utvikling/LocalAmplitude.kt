a986adea31bc34a4c9503da25fee2f9d3e135993
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-02 07:17:28 +0200

    Lokal amplitude api (#618)

---
+++ src/test/kotlin/no/nav/helse/flex/utvikling/LocalAmplitude.kt
@@ -0,0 +1,65 @@
+package no.nav.helse.flex.utvikling
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.flex.util.OBJECT_MAPPER
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.context.annotation.Profile
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@Profile("dev")
+class LocalAmplitude {
+    val requests = mutableListOf<JsonNode>()
+
+    @PostMapping(value = ["/api/amplitude"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Unprotected
+    fun amplitudePost(
+        @RequestBody request: String,
+    ) {
+        val rootNode = OBJECT_MAPPER.readTree(request)
+
+        requests.add(rootNode)
+    }
+
+    @GetMapping(value = ["/api/amplitude"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @CrossOrigin
+    @Unprotected
+    fun amplitudeGet(): Response {
+        return Response(
+            requests,
+            AmplitudeUserdata(
+                userId = "123",
+                firstUsed = "2021-01-01",
+                lastUsed = "2021-01-01",
+                device = "device",
+                os = "os",
+                properties =
+                    Properties(
+                        vindusHoyde = 100,
+                        vindusBredde = 100,
+                    ),
+            ),
+        )
+    }
+
+    data class AmplitudeUserdata(
+        @JsonProperty("user_id") val userId: String,
+        @JsonProperty("first_used") val firstUsed: String,
+        @JsonProperty("last_used") val lastUsed: String,
+        val device: String,
+        val os: String,
+        val properties: Properties,
+    )
+
+    data class Properties(
+        @JsonProperty("vindushoyde") val vindusHoyde: Int,
+        @JsonProperty("vindusbredde") val vindusBredde: Int,
+    )
+
+    data class Response(val events: List<JsonNode>, val userData: AmplitudeUserdata)
+}
