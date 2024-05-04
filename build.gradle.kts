23e9271bf48afe3a192a0c99841b9e9b6439d1f7
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2024-05-04 08:27:24 +0200

    Bump confluentVersion from 7.6.0 to 7.6.1 (#619)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 }
 
 val tokenSupportVersion = "4.1.4"
-val confluentVersion = "7.6.0"
+val confluentVersion = "7.6.1"
 val syfoKafkaVersion = "2021.07.20-09.39-6be2c52c"
 val sykepengesoknadKafkaVersion = "2024.03.21-14.13-5011349f"
 val mockitoKotlinVersion = "2.2.0"
@@ -82,6 +82,7 @@ dependencies {
     testImplementation("com.networknt:json-schema-validator:$jsonSchemaValidatorVersion")
     testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("commons-codec:commons-codec")
 }
 
 kotlin {
--- src/test/kotlin/no/nav/helse/flex/testoppsett/Kafka.kt
+++ src/test/kotlin/no/nav/helse/flex/testoppsett/Kafka.kt
@@ -4,7 +4,7 @@ import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 
 fun startKafkaContainer() {
-    KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.5.3")).apply {
+    KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1")).apply {
         start()
         System.setProperty("KAFKA_BROKERS", bootstrapServers)
     }
