d7f1cc36f732ac08831bd52813d062508d8bcad7
golshidbahadorian <golshid.bahadorian@nav.no>
2024-06-03 15:30:50 +0200

    Renamer utland filen til OppholdUtenforEOS

--- src/main/kotlin/no/nav/helse/flex/domain/mapper/sporsmalprossesering/SoktSykepengerUnderUtlandsopphold.kt
+++ src/main/kotlin/no/nav/helse/flex/domain/mapper/sporsmalprossesering/SoktSykepengerUnderUtlandsopphold.kt
@@ -2,12 +2,7 @@ package no.nav.helse.flex.domain.mapper.sporsmalprossesering
 
 import no.nav.helse.flex.domain.Sykepengesoknad
 import no.nav.helse.flex.soknadsopprettelse.OPPHOLD_UTENFOR_EOS
-import no.nav.helse.flex.soknadsopprettelse.UTLANDSOPPHOLD_SOKT_SYKEPENGER
 
 fun harSoktSykepengerUnderUtlandsopphold(sykepengesoknad: Sykepengesoknad): Boolean {
-    return if (sykepengesoknad.getSporsmalMedTagOrNull(OPPHOLD_UTENFOR_EOS)?.forsteSvar !== "CHECKED") {
-        false
-    } else {
-        sykepengesoknad.getSporsmalMedTagOrNull(UTLANDSOPPHOLD_SOKT_SYKEPENGER)?.forsteSvar == "JA"
-    }
+    return sykepengesoknad.getSporsmalMedTagOrNull(OPPHOLD_UTENFOR_EOS)?.forsteSvar == "JA"
 }
