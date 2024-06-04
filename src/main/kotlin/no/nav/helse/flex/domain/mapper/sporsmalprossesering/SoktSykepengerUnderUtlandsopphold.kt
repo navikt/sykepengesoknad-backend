1706a5a2526509b1f839faacfc6c28a2d8eef88c
golshidbahadorian <golshid.bahadorian@nav.no>
2024-06-04 09:52:23 +0200

    Bytter sjekk fra "JA" tilbake til "CHECKED"

--- src/main/kotlin/no/nav/helse/flex/domain/mapper/sporsmalprossesering/SoktSykepengerUnderUtlandsopphold.kt
+++ src/main/kotlin/no/nav/helse/flex/domain/mapper/sporsmalprossesering/SoktSykepengerUnderUtlandsopphold.kt
@@ -4,5 +4,5 @@ import no.nav.helse.flex.domain.Sykepengesoknad
 import no.nav.helse.flex.soknadsopprettelse.OPPHOLD_UTENFOR_EOS
 
 fun harSoktSykepengerUnderUtlandsopphold(sykepengesoknad: Sykepengesoknad): Boolean {
-    return sykepengesoknad.getSporsmalMedTagOrNull(OPPHOLD_UTENFOR_EOS)?.forsteSvar == "JA"
+    return sykepengesoknad.getSporsmalMedTagOrNull(OPPHOLD_UTENFOR_EOS)?.forsteSvar == "CHECKED"
 }
