e8a60808568b120d8b7b19ae091d02b7810b45d2
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-06-12 13:19:03 +0200

    Returner spørsmål om medlemskap i UtviklingApplication

--- src/test/kotlin/no/nav/helse/flex/utvikling/UtviklingApplication.kt
+++ src/test/kotlin/no/nav/helse/flex/utvikling/UtviklingApplication.kt
@@ -1,14 +1,22 @@
 package no.nav.helse.flex.utvikling
 
 import no.nav.helse.flex.Application
+import no.nav.helse.flex.FellesTestOppsett.Companion.medlemskapMockWebServer
+import no.nav.helse.flex.medlemskap.KjentOppholdstillatelse
+import no.nav.helse.flex.medlemskap.MedlemskapVurderingResponse
+import no.nav.helse.flex.medlemskap.MedlemskapVurderingSporsmal
+import no.nav.helse.flex.medlemskap.MedlemskapVurderingSvarType
 import no.nav.helse.flex.mockdispatcher.FlexSyketilfelleMockDispatcher
 import no.nav.helse.flex.testoppsett.startAlleContainere
 import no.nav.helse.flex.testoppsett.startMockWebServere
+import no.nav.helse.flex.util.serialisertTilString
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import java.time.LocalDate
 
 @Configuration
 @EnableMockOAuth2Server
@@ -18,6 +26,27 @@ class AuthConfig
 fun main(args: Array<String>) {
     startAlleContainere()
     startMockWebServere()
+
+    // Stiller alle spørsmål om medlemskap i UtviklingApplication.
+    medlemskapMockWebServer.enqueue(
+        MockResponse().setResponseCode(200).setBody(
+            MedlemskapVurderingResponse(
+                svar = MedlemskapVurderingSvarType.UAVKLART,
+                sporsmal =
+                    listOf(
+                        MedlemskapVurderingSporsmal.OPPHOLDSTILATELSE,
+                        MedlemskapVurderingSporsmal.ARBEID_UTENFOR_NORGE,
+                        MedlemskapVurderingSporsmal.OPPHOLD_UTENFOR_NORGE,
+                        MedlemskapVurderingSporsmal.OPPHOLD_UTENFOR_EØS_OMRÅDE,
+                    ),
+                kjentOppholdstillatelse =
+                    KjentOppholdstillatelse(
+                        fom = LocalDate.now().minusMonths(1),
+                        tom = LocalDate.now().plusMonths(2),
+                    ),
+            ).serialisertTilString(),
+        ),
+    )
 
     // Denne har ikke MockWebServer i testene
     MockWebServer().apply {
