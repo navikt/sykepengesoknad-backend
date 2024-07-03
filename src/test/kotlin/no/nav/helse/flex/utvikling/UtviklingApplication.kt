329d1eea303df560cd8856579a69f004fbda0104
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-07-03 10:58:00 +0200

    Ikke start MockWebServer og Testcontainers to ganger

--- src/test/kotlin/no/nav/helse/flex/utvikling/UtviklingApplication.kt
+++ src/test/kotlin/no/nav/helse/flex/utvikling/UtviklingApplication.kt
@@ -2,8 +2,6 @@ package no.nav.helse.flex.utvikling
 
 import no.nav.helse.flex.Application
 import no.nav.helse.flex.mockdispatcher.FlexSyketilfelleMockDispatcher
-import no.nav.helse.flex.testoppsett.startAlleContainere
-import no.nav.helse.flex.testoppsett.startMockWebServere
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import okhttp3.mockwebserver.MockWebServer
 import org.springframework.boot.runApplication
@@ -16,9 +14,6 @@ import org.springframework.context.annotation.Profile
 class AuthConfig
 
 fun main(args: Array<String>) {
-    startAlleContainere()
-    startMockWebServere()
-
     // Denne har ikke MockWebServer i testene
     MockWebServer().apply {
         System.setProperty("flex.syketilfelle.url", "http://localhost:$port")
