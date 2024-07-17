598317cad8bd1390ad76915acb970b13c075c2dd
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-07-17 15:27:06 +0200

    mock for sigrun

---
+++ src/test/kotlin/no/nav/helse/flex/soknadsopprettelse/PensjonsgivendeInntektHentingTest.kt
@@ -0,0 +1,20 @@
+package no.nav.helse.flex.soknadsopprettelse
+
+import no.nav.helse.flex.FellesTestOppsett
+import no.nav.helse.flex.client.inntektskomponenten.PensjongivendeInntektClient
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class PensjonsgivendeInntektHentingTest : FellesTestOppsett() {
+    @Autowired
+    lateinit var pensjongivendeInntektClient: PensjongivendeInntektClient
+
+    @Test
+    fun `henter ut pensjonsgivende inntekt for fnr`() {
+        pensjongivendeInntektClient.hentPensjonsgivendeInntekter(fnr = "11111234565").let {
+            it.size `should be equal to` 3
+            it.first().pensjonsgivendeInntekt.pensjonsgivendeInntektAvNaeringsinntekt `should be equal to` 300000
+        }
+    }
+}
