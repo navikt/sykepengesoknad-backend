9e43acb71c4f9cf8f45cc7219632fbae94638d02
golshidbahadorian <golshid.bahadorian@nav.no>
2024-07-18 13:37:46 +0200

    Prøver å gjøre kall for at år foreløpig

--- src/main/kotlin/no/nav/helse/flex/client/inntektskomponenten/PensjongivendeInntektClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/inntektskomponenten/PensjongivendeInntektClient.kt
@@ -7,7 +7,6 @@ import org.springframework.http.*
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
-import java.time.LocalDate
 import java.util.*
 
 @Component
@@ -18,7 +17,7 @@ class PensjongivendeInntektClient(
 ) {
     val log = logger()
 
-    fun hentPensjonsgivendeInntekter(fnr: String): List<HentPensjonsgivendeInntektResponse> {
+    fun hentPensjonsgivendeInntekter(fnr: String): HentPensjonsgivendeInntektResponse? {
         val uriBuilder =
             UriComponentsBuilder.fromHttpUrl("$url/api/v1/pensjonsgivendeinntektforfolketrygden")
 
@@ -27,40 +26,38 @@ class PensjongivendeInntektClient(
         headers["Nav-Call-Id"] = UUID.randomUUID().toString()
         headers["rettighetspakke"] = "navSykepenger"
         headers["Nav-Personident"] = fnr
+        headers["inntektsaar"] = "2023"
         headers.contentType = MediaType.APPLICATION_JSON
         headers.accept = listOf(MediaType.APPLICATION_JSON)
 
-        val treSisteAar = (LocalDate.now().year downTo LocalDate.now().minusYears(2).year).map { it.toString() }
-        log.info("Tre siste år: ${treSisteAar.serialisertTilString()}")
+        // val treSisteAar = (LocalDate.now().year downTo LocalDate.now().minusYears(2).year).map { it.toString() }
+        // log.info("Tre siste år: ${treSisteAar.serialisertTilString()}")
 
-        val pensjonsgivendeInntektSisteTreAar =
-            treSisteAar.map { aar ->
-                headers["inntektsaar"] = aar
-                val result =
-                    persongivendeInntektRestTemplate
-                        .exchange(
-                            uriBuilder.toUriString(),
-                            HttpMethod.GET,
-                            HttpEntity<Any>(headers),
-                            HentPensjonsgivendeInntektResponse::class.java,
-                        )
-                if (result.statusCode != HttpStatus.OK) {
-                    val message = "Kall mot Sigrun feiler med HTTP-" + result.statusCode
-                    log.error(message)
-                    throw RuntimeException(message)
-                }
-
-                log.info("Kall mot Sigrun result: ${result.serialisertTilString()}")
-                result.body?.let {
-                    log.info("Kall mot Sigrun result body: ${it.serialisertTilString()}")
-                    return@map it
-                }
-
-                val message = "Kall mot Sigrun returnerer ikke data"
+        val result: ResponseEntity<HentPensjonsgivendeInntektResponse>
+        try {
+             result =
+                persongivendeInntektRestTemplate
+                    .exchange(
+                        uriBuilder.toUriString(),
+                        HttpMethod.GET,
+                        HttpEntity<Any>(headers),
+                        HentPensjonsgivendeInntektResponse::class.java,
+                    )
+            if (result.statusCode != HttpStatus.OK) {
+                val message = "Kall mot Sigrun feiler med HTTP-" + result.statusCode
                 log.error(message)
                 throw RuntimeException(message)
             }
 
-        return pensjonsgivendeInntektSisteTreAar
+            log.info("Kall mot Sigrun result: ${result.serialisertTilString()}")
+            result.body?.let {
+                return it
+            }
+        } catch (e: Exception) {
+            val message = "Kall mot Sigrun returnerer ikke data"
+            log.error(message + e)
+            throw RuntimeException(message)
+        }
+        return result.body!!
     }
 }
