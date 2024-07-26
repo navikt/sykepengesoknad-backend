92493df23a24067730efb7926535f9a163c7f735
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-07-26 15:38:55 +0200

    wip

--- src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/sporsmal/Inntektsopplysninger.kt
+++ src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/sporsmal/Inntektsopplysninger.kt
@@ -36,8 +36,8 @@ fun lagSporsmalOmInntektsopplyninger(soknad: Sykepengesoknad): Sporsmal {
                                 sporsmalstekst = "Er du ny i arbeidslivet etter ${
                                     DatoUtil.formatterDato(
                                         datoForVarigEndringEtter,
-                                    )
-                                }?",
+                                    )}?",
+
                                 svartype = Svartype.RADIO_GRUPPE,
                                 undersporsmal =
                                     listOf(
