91d8a0809be9bf5cc53df155d140bfe0484d1c05
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-08-27 10:45:32 +0200

    beregner sykepengegrunnlag for inntekt over 6G

--- src/main/kotlin/no/nav/helse/flex/util/UtregningUtil.kt
+++ src/main/kotlin/no/nav/helse/flex/util/UtregningUtil.kt
@@ -12,3 +12,32 @@ fun sykepengegrunnlagUtregner(
     gPaaSykmeldingstidspunktet: BigInteger,
     gjennomsnittligGIKalenderaaret: BigInteger,
 ) = (pensjonsgivendeInntektIKalenderAaret * gPaaSykmeldingstidspunktet) / gjennomsnittligGIKalenderaaret
+
+/**
+ * Finner gennomsnittlig inntekt og justerer basert på regler i rundskriv.
+ * Dersom snittet er over 6G fastsettes sykepengegrunnlaget til 6G
+ */
+fun beregnGjennomsnittligInntekt(
+    beregnetInntektPerAar: Map<String, BigInteger>,
+    grunnbeloepSykmldTidspunkt: Int,
+): Pair<BigInteger, BigInteger> {
+    val justerteInntekter = beregnetInntektPerAar.toMutableMap()
+    val g12 = grunnbeloepSykmldTidspunkt.toBigInteger() * 12.toBigInteger()
+    val g6 = grunnbeloepSykmldTidspunkt.toBigInteger() * 6.toBigInteger()
+
+    val verdierPaa12G =
+        beregnetInntektPerAar.filterValues { it >= g12 }.map {
+            it.key to g12
+        }.toMap()
+    justerteInntekter.putAll(verdierPaa12G)
+
+    val reduserteVerdierMellom6og12G =
+        justerteInntekter.filterValues { it in g6..g12 }.map {
+            it.key to g6 + (it.value - g6) / 3.toBigInteger()
+        }
+    justerteInntekter.putAll(reduserteVerdierMellom6og12G)
+
+    val snittVerdi = justerteInntekter.values.sumOf { it } / 3.toBigInteger()
+
+    return Pair(snittVerdi, g6)
+}
--- src/test/kotlin/no/nav/helse/flex/service/SykepengegrunnlagServiceTest.kt
+++ src/test/kotlin/no/nav/helse/flex/service/SykepengegrunnlagServiceTest.kt
@@ -27,7 +27,31 @@ class SykepengegrunnlagServiceTest : FellesTestOppsett() {
 
         grunnlagVerdier `should not be` null
         grunnlagVerdier!!.let {
+            it.fastsattSykepengegrunnlag `should be equal to` 372758.toBigInteger()
             it.gjennomsnittTotal `should be equal to` 372758.toBigInteger()
+            it.grunnbeloepPerAar.size `should be equal to` 3
+            it.gjennomsnittPerAar.size `should be equal to` 3
+        }
+    }
+
+    @Test
+    fun `sjekker utregning for inntekt mellom 6G og 12G`() {
+        val soknad =
+            opprettNyNaeringsdrivendeSoknad().copy(
+                fnr = "87654321234",
+                startSykeforlop = LocalDate.now(),
+                fom = LocalDate.now().minusDays(30),
+                tom = LocalDate.now().minusDays(1),
+                sykmeldingSkrevet = Instant.now(),
+                aktivertDato = LocalDate.now().minusDays(30),
+            )
+
+        val grunnlagVerdier = sykepengegrunnlagService.sykepengegrunnlagNaeringsdrivende(soknad)
+
+        grunnlagVerdier `should not be` null
+        grunnlagVerdier!!.let {
+            it.fastsattSykepengegrunnlag `should be equal to` 744168.toBigInteger()
+            it.gjennomsnittTotal `should be equal to` 771106.toBigInteger()
             it.grunnbeloepPerAar.size `should be equal to` 3
             it.gjennomsnittPerAar.size `should be equal to` 3
         }
