89f3952a33854574290c37ac538b23d5c5d8ebc5
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-08-27 14:23:11 +0200

    AAREG v2 med POST (#709)

--- src/test/kotlin/no/nav/helse/flex/mockdispatcher/AaregMockDispatcher.kt
+++ src/test/kotlin/no/nav/helse/flex/mockdispatcher/AaregMockDispatcher.kt
@@ -5,18 +5,10 @@ import no.nav.helse.flex.util.serialisertTilString
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.QueueDispatcher
 import okhttp3.mockwebserver.RecordedRequest
-import java.time.LocalDate
 
 object AaregMockDispatcher : QueueDispatcher() {
     override fun dispatch(request: RecordedRequest): MockResponse {
-        val response =
-            listOf(
-                Arbeidsforhold(
-                    arbeidsgiver = Arbeidsgiver("vanlig", "1234"),
-                    ansettelsesperiode = Ansettelsesperiode(Periode(LocalDate.now(), LocalDate.now())),
-                    opplysningspliktig = Opplysningspliktig("vanlig", "1234"),
-                ),
-            )
+        val response = ArbeidsforholdoversiktResponse(arbeidsforholdoversikter = emptyList())
         return MockResponse()
             .setResponseCode(200)
             .setBody(response.serialisertTilString())
