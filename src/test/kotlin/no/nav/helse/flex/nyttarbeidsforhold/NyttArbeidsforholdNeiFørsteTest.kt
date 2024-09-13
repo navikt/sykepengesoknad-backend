0bebe5869b40292070ac9e4a46aec95974d23dcd
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-09-13 15:31:32 +0200

    Test av nei

---
+++ src/test/kotlin/no/nav/helse/flex/nyttarbeidsforhold/NyttArbeidsforholdNeiFørsteTest.kt
@@ -0,0 +1,70 @@
+package no.nav.helse.flex.nyttarbeidsforhold
+
+import no.nav.helse.flex.*
+import no.nav.helse.flex.aktivering.SoknadAktivering
+import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstatus
+import no.nav.helse.flex.domain.Arbeidssituasjon
+import no.nav.helse.flex.soknadsopprettelse.*
+import no.nav.helse.flex.soknadsopprettelse.sporsmal.medlemskap.medIndex
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
+import no.nav.helse.flex.testdata.heltSykmeldt
+import no.nav.helse.flex.testdata.sykmeldingKafkaMessage
+import no.nav.helse.flex.testutil.SoknadBesvarer
+import no.nav.syfo.sykmelding.kafka.model.ArbeidsgiverStatusKafkaDTO
+import org.amshove.kluent.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDate
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class NyttArbeidsforholdNeiFørsteTest : FellesTestOppsett() {
+    private val fnr = "22222220001"
+    private final val basisdato = LocalDate.now().minusDays(1)
+
+    @BeforeAll
+    fun konfigurerUnleash() {
+        fakeUnleash.resetAll()
+        fakeUnleash.enable("sykepengesoknad-backend-tilkommen-inntekt")
+    }
+
+    @Test
+    @Order(1)
+    fun `Arbeidstakersøknader opprettes for en lang sykmelding`() {
+        sendSykmelding(
+            sykmeldingKafkaMessage(
+                fnr = fnr,
+                sykmeldingsperioder =
+                heltSykmeldt(
+                    fom = basisdato.minusDays(20),
+                    tom = basisdato,
+                ),
+                arbeidsgiver = ArbeidsgiverStatusKafkaDTO(orgnummer = "123454543", orgNavn = "MATBUTIKKEN AS"),
+            ),
+        )
+    }
+
+
+    @Test
+    @Order(3)
+    fun `Vi besvarer og sender inn søknaden med nei`() {
+        flexSyketilfelleMockRestServiceServer.reset()
+        mockFlexSyketilfelleArbeidsgiverperiode()
+        val soknaden = hentSoknader(fnr = fnr).first()
+
+        val sendtSoknad =
+            SoknadBesvarer(rSSykepengesoknad = soknaden, mockMvc = this, fnr = fnr)
+                .standardSvar()
+                .besvarSporsmal(tag = NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG, svar = "NEI").sendSoknad()
+        assertThat(sendtSoknad.status).isEqualTo(RSSoknadstatus.SENDT)
+
+        val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
+        kafkaSoknader.shouldHaveSize(1)
+
+        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.SENDT
+        kafkaSoknader[0].inntektFraNyttArbeidsforhold!!.shouldBeEmpty()
+
+        juridiskVurderingKafkaConsumer.ventPåRecords(antall = 2)
+    }
+
+}
