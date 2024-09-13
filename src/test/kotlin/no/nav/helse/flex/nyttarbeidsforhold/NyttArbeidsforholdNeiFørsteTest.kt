ec230cb7c48e0f7f9a98403f0c3def2916ce98cd
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-09-13 15:41:58 +0200

    Test av nei svar på nytt arbeidsforhold (#729)
    * Test av nei
    
    * Test av neisvar
    
    * Felles oppsett for nytt arbeidsforhold test

---
+++ src/test/kotlin/no/nav/helse/flex/nyttarbeidsforhold/NyttArbeidsforholdNeiFørsteTest.kt
@@ -0,0 +1,35 @@
+package no.nav.helse.flex.nyttarbeidsforhold
+
+import no.nav.helse.flex.*
+import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstatus
+import no.nav.helse.flex.soknadsopprettelse.*
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
+import no.nav.helse.flex.testutil.SoknadBesvarer
+import org.amshove.kluent.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.*
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class NyttArbeidsforholdNeiFørsteTest : NyttArbeidsforholdFellesOppsett() {
+    @Test
+    @Order(3)
+    fun `Vi besvarer og sender inn søknaden med nei`() {
+        flexSyketilfelleMockRestServiceServer.reset()
+        mockFlexSyketilfelleArbeidsgiverperiode()
+        val soknaden = hentSoknader(fnr = fnr).first()
+
+        val sendtSoknad =
+            SoknadBesvarer(rSSykepengesoknad = soknaden, mockMvc = this, fnr = fnr)
+                .standardSvar()
+                .besvarSporsmal(tag = NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG, svar = "NEI").sendSoknad()
+        assertThat(sendtSoknad.status).isEqualTo(RSSoknadstatus.SENDT)
+
+        val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
+        kafkaSoknader.shouldHaveSize(1)
+
+        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.SENDT
+        kafkaSoknader[0].inntektFraNyttArbeidsforhold!!.shouldBeEmpty()
+
+        juridiskVurderingKafkaConsumer.ventPåRecords(antall = 2)
+    }
+}
