6312100e611845553dbe2fbe7df115660a6a0999
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-09-16 12:57:10 +0200

    drodling

--- src/main/kotlin/no/nav/helse/flex/arbeidsgiverperiode/ErUtenforArbeidsgiverPeriode.kt
+++ src/main/kotlin/no/nav/helse/flex/arbeidsgiverperiode/ErUtenforArbeidsgiverPeriode.kt
@@ -1,9 +1,38 @@
 package no.nav.helse.flex.arbeidsgiverperiode
 
+import no.nav.helse.flex.domain.Periode
 import no.nav.helse.flex.domain.Sykepengesoknad
+import java.time.LocalDate
 
 fun Sykepengesoknad.erUtenforArbeidsgiverPeriode(andreSoknader: List<Sykepengesoknad>): Boolean {
     val sykmeldtFraArbeidsgiverOrgnr = this.arbeidsgiverOrgnummer
 
-    return true
+    val alleDager = andreSoknader
+        .filter { it.arbeidsgiverOrgnummer == sykmeldtFraArbeidsgiverOrgnr }
+        .skapDager()
+
+    val arbeidsgiverperioder = alleDager.beregnArbeidsgiverperioder()
+
+    return !arbeidsgiverperioder.any { it.overlapper(this.fom, this.tom) }
+
+}
+
+private fun List<DagOgType>.beregnArbeidsgiverperioder(): List<Periode> {
+    return listOf(Periode(LocalDate.now(), LocalDate.now().plusDays(1)))
+}
+
+
+enum class DagType{
+    ARBEIDSDAG,  FERIEDAG, PERMISJONSDAG, SYKEDAG, EGENMELDING}
+
+data class DagOgType(
+    val dag: LocalDate,
+    val type: DagType
+
+)
+
+fun List<Sykepengesoknad>.skapDager(): List<DagOgType> {
+
+    return listOf(DagOgType(LocalDate.now(), DagType.FERIEDAG), DagOgType(LocalDate.now().plusDays(1), DagType.ARBEIDSDAG))
+
 }
