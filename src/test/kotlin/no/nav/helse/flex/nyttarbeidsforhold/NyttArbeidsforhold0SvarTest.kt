233bac4610a177cb5584e88ad3830d9e69f64259
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-09-16 08:42:19 +0200

    Test av 0 svar (#732)

---
+++ src/test/kotlin/no/nav/helse/flex/nyttarbeidsforhold/NyttArbeidsforhold0SvarTest.kt
@@ -0,0 +1,58 @@
+package no.nav.helse.flex.nyttarbeidsforhold
+
+import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstatus
+import no.nav.helse.flex.hentSoknader
+import no.nav.helse.flex.mockFlexSyketilfelleArbeidsgiverperiode
+import no.nav.helse.flex.soknadsopprettelse.NYTT_ARBEIDSFORHOLD_UNDERVEIS_BRUTTO
+import no.nav.helse.flex.soknadsopprettelse.NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG
+import no.nav.helse.flex.soknadsopprettelse.NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG_FORSTE_ARBEIDSDAG
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
+import no.nav.helse.flex.testutil.SoknadBesvarer
+import no.nav.helse.flex.tilSoknader
+import no.nav.helse.flex.ventPåRecords
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should be true`
+import org.amshove.kluent.shouldHaveSize
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class NyttArbeidsforhold0SvarTest : NyttArbeidsforholdFellesOppsett() {
+    @Test
+    @Order(2)
+    fun `Vi besvarer og sender inn søknaden med 0 svar`() {
+        flexSyketilfelleMockRestServiceServer.reset()
+        mockFlexSyketilfelleArbeidsgiverperiode()
+        val soknaden = hentSoknader(fnr = fnr).first()
+
+        val sendtSoknad =
+            SoknadBesvarer(rSSykepengesoknad = soknaden, mockMvc = this, fnr = fnr)
+                .standardSvar()
+                .besvarSporsmal(tag = NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG, svar = "JA", ferdigBesvart = false)
+                .besvarSporsmal(
+                    tag = NYTT_ARBEIDSFORHOLD_UNDERVEIS_FORSTEGANG_FORSTE_ARBEIDSDAG,
+                    svar = basisdato.minusDays(4).toString(),
+                    ferdigBesvart = false,
+                )
+                .besvarSporsmal(tag = NYTT_ARBEIDSFORHOLD_UNDERVEIS_BRUTTO, svar = "0", ferdigBesvart = true)
+                .sendSoknad()
+        assertThat(sendtSoknad.status).isEqualTo(RSSoknadstatus.SENDT)
+
+        val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
+        kafkaSoknader.shouldHaveSize(1)
+
+        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.SENDT
+        kafkaSoknader[0].inntektFraNyttArbeidsforhold!!.shouldHaveSize(1)
+
+        val inntektFraNyttArbeidsforhold = kafkaSoknader[0].inntektFraNyttArbeidsforhold!!.first()
+        inntektFraNyttArbeidsforhold.fom `should be equal to` basisdato.minusDays(4)
+        inntektFraNyttArbeidsforhold.tom `should be equal to` basisdato
+        inntektFraNyttArbeidsforhold.forstegangssporsmal.`should be true`()
+        inntektFraNyttArbeidsforhold.belopPerDag `should be equal to` 0
+
+        juridiskVurderingKafkaConsumer.ventPåRecords(antall = 2)
+    }
+}
