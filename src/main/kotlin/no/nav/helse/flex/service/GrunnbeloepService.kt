88a764ff9449f8329b3913b52acf5468dd369525
golshidbahadorian <golshid.bahadorian@nav.no>
2024-09-18 09:31:43 +0200

    henter G, 5 år tilbake fra og med i år

--- src/main/kotlin/no/nav/helse/flex/service/GrunnbeloepService.kt
+++ src/main/kotlin/no/nav/helse/flex/service/GrunnbeloepService.kt
@@ -10,29 +10,14 @@ import java.time.LocalDate
 @Service
 @CacheConfig
 class GrunnbeloepService(private val grunnbeloepClient: GrunnbeloepClient) {
-    fun getGrunnbeloep(dato: LocalDate?): Mono<GrunnbeloepResponse>? {
-        return grunnbeloepClient.getGrunnbeloep(dato)
-    }
 
     fun getHistorikk(fra: LocalDate?): Mono<List<GrunnbeloepResponse>> {
         return grunnbeloepClient.getHistorikk(fra)
     }
 
-    fun hentGrunnbeloepForFemAarSiden(): Mono<List<GrunnbeloepResponse>> {
-        val detteAaret = LocalDate.now().year
-        val sisteFemAar =
-            (detteAaret - 5 until detteAaret).map {
-                getGrunnbeloep(LocalDate.of(it, 1, 1))
-            }
-
-        return Mono.zip(sisteFemAar) { results ->
-            results.map { it as GrunnbeloepResponse }
-        }
-    }
-
     // TODO: Cache
     fun hentHistorikkSisteFemAar(): Mono<List<GrunnbeloepResponse>> {
-        val femAarSiden = LocalDate.of(2024, 1, 1).minusYears(5)
+        val femAarSiden = LocalDate.now().minusYears(5)
         return getHistorikk(femAarSiden)
     }
 }
