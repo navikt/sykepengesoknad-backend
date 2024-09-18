7342d98288d0647bb929a6eadb94330c19394a84
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-09-18 09:09:49 +0200

    fleresettings

--- src/test/kotlin/no/nav/helse/flex/testoppsett/Kafka.kt
+++ src/test/kotlin/no/nav/helse/flex/testoppsett/Kafka.kt
@@ -6,6 +6,8 @@ import org.testcontainers.utility.DockerImageName
 fun startKafkaContainer() {
     KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1")).apply {
         withEnv("KAFKA_LOG_FLUSH_INTERVAL_MESSAGES", "1")
+        withEnv("KAFKA_DEFAULT_REPLICATION_FACTOR", "1")
+        withEnv("KAFKA_NUM_PARTITIONS", "1")
         withEnv("KAFKA_MIN_INSYNC_REPLICAS", "1")
         withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
         start()
