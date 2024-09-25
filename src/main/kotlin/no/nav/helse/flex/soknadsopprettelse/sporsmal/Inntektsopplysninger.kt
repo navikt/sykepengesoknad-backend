c81ffbb6675acdf4f1800e8175a3080f3e9156bc
golshidbahadorian <golshid.bahadorian@nav.no>
2024-09-25 15:51:55 +0200

    Tar tilbake den gamle spørsmålsteksten

--- src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/sporsmal/Inntektsopplysninger.kt
+++ src/main/kotlin/no/nav/helse/flex/soknadsopprettelse/sporsmal/Inntektsopplysninger.kt
@@ -37,7 +37,7 @@ fun lagSporsmalOmInntektsopplyninger(
                         listOf(
                             Sporsmal(
                                 tag = INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET,
-                                sporsmalstekst = "Utenom virksomheten din, har du hatt andre jobber før ${
+                                sporsmalstekst = "Er du ny i arbeidslivet etter ${
                                     DatoUtil.formatterDato(
                                         datoForVarigEndringEtter,
                                     )
