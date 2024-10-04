94d18695ed0bcbd296a6c83c42e52d7d7fd22c01
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2024-10-04 12:33:04 +0200

    bruker år fra ferdiglignet inntekt i spørsmål om inntekt for selvstendig næringsdrivende, om vi har det (#782)

--- src/test/kotlin/no/nav/helse/flex/sigrun/SigrunToggleTest.kt
+++ src/test/kotlin/no/nav/helse/flex/sigrun/SigrunToggleTest.kt
@@ -42,7 +42,8 @@ class SigrunToggleTest : FellesTestOppsett() {
 
         soknader shouldHaveSize 1
 
-        hentSoknader("87654321234").first().sporsmal!!.flatten().first {
+        val soknad = hentSoknader("87654321234").first()
+        soknad.sporsmal!!.flatten().first {
             it.tag == "INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT"
         }.metadata `should be equal to`
             objectMapper.readTree(
@@ -50,10 +51,14 @@ class SigrunToggleTest : FellesTestOppsett() {
                 {"sigrunInntekt":{"inntekter":[{"aar":"2023","verdi":1067008},{"aar":"2022","verdi":1129745},{"aar":"2021","verdi":1184422}],"g-verdier":[{"aar":"2021","verdi":104716},{"aar":"2022","verdi":109784},{"aar":"2023","verdi":116239}],"g-sykmelding":124028,"beregnet":{"snitt":871798,"p25":930210,"m25":558126}}}
                 """.trimIndent(),
             )
+
+        soknad.sporsmal!!.flatten().first {
+            it.tag == "INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET"
+        }.sporsmalstekst `should be equal to` "Er du ny i arbeidslivet etter 1. januar 2021?"
     }
 
     @Test
-    fun `metadata på spørsmål om varigendring må være null når toggle er på`() {
+    fun `metadata på spørsmål om varigendring må være null når toggle er av`() {
         fakeUnleash.disable("sykepengesoknad-backend-sigrun")
         val soknader =
             sendSykmelding(
@@ -70,9 +75,14 @@ class SigrunToggleTest : FellesTestOppsett() {
 
         soknader shouldHaveSize 1
 
-        hentSoknader("87654321234").first().sporsmal!!.flatten().first {
+        val soknad = hentSoknader("87654321234").first()
+        soknad.sporsmal!!.flatten().first {
             it.tag == "INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT"
         }.metadata.toString() `should be equal to` "null"
+
+        soknad.sporsmal!!.flatten().first {
+            it.tag == "INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET"
+        }.sporsmalstekst `should be equal to` "Er du ny i arbeidslivet etter 1. januar 2019?"
     }
 
     @Test
