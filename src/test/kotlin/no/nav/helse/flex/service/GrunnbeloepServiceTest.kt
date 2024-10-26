db0802560cebd527ccfd7b061116a02b08ea2c1a
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-26 15:43:15 +0200

    refaktorering

--- src/test/kotlin/no/nav/helse/flex/service/GrunnbeloepServiceTest.kt
+++ src/test/kotlin/no/nav/helse/flex/service/GrunnbeloepServiceTest.kt
@@ -1,20 +1,11 @@
 package no.nav.helse.flex.service
 
 import no.nav.helse.flex.FellesTestOppsett
-import no.nav.helse.flex.client.grunnbeloep.GrunnbeloepClient
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.SpyBean
 import java.time.LocalDate
 
 class GrunnbeloepServiceTest : FellesTestOppsett() {
-    @Autowired
-    lateinit var grunnbeloepService: GrunnbeloepService
-
-    @SpyBean
-    private lateinit var grunnbeloepClient: GrunnbeloepClient
-
     @Test
     fun `skal bruke cache etter første kall`() {
         val fraDato = LocalDate.of(2024, 1, 1)
