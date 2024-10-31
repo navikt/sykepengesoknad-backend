bc982fede9025531203478db97dd9f96bd7b2ef8
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-10-31 10:01:11 +0100

    Fjern Disabled på SigrunToggleTest

--- src/test/kotlin/no/nav/helse/flex/sigrun/SigrunToggleTest.kt
+++ src/test/kotlin/no/nav/helse/flex/sigrun/SigrunToggleTest.kt
@@ -2,7 +2,6 @@ package no.nav.helse.flex.sigrun
 
 import no.nav.helse.flex.*
 import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstatus
-import no.nav.helse.flex.controller.domain.sykepengesoknad.flatten
 import no.nav.helse.flex.domain.Arbeidssituasjon
 import no.nav.helse.flex.soknadsopprettelse.*
 import no.nav.helse.flex.soknadsopprettelse.sporsmal.medlemskap.medIndex
@@ -14,7 +13,6 @@ import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should not be`
 import org.amshove.kluent.shouldHaveSize
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -25,9 +23,8 @@ class SigrunToggleTest : FellesTestOppsett() {
         fakeUnleash.resetAll()
     }
 
-    @Disabled
     @Test
-    fun `metadata på spørsmål om varig endring må ha verdi når toggle er på`() {
+    fun `Metadata på spørsmål om varig endring har inntektsopplysniner fra Sigrun når toggle er på`() {
         fakeUnleash.enable("sykepengesoknad-backend-sigrun")
         val soknader =
             sendSykmelding(
@@ -44,20 +41,15 @@ class SigrunToggleTest : FellesTestOppsett() {
 
         soknader shouldHaveSize 1
 
-        val soknad = hentSoknader("87654321234").first()
-        soknad.sporsmal!!.flatten().first {
+        soknader.first().sporsmal!!.flatten().first {
             it.tag == "INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT"
         }.metadata `should not be` null
-
-        soknad.sporsmal!!.flatten().first {
-            it.tag == "INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET"
-        }.sporsmalstekst `should be equal to` "Er du ny i arbeidslivet etter 1. januar 2021?"
     }
 
-    @Disabled
     @Test
-    fun `metadata på spørsmål om varigendring må være null når toggle er av`() {
+    fun `Metadata på spørsmål om varig endring er null når toggle er av`() {
         fakeUnleash.disable("sykepengesoknad-backend-sigrun")
+
         val soknader =
             sendSykmelding(
                 sykmeldingKafkaMessage(
@@ -73,19 +65,13 @@ class SigrunToggleTest : FellesTestOppsett() {
 
         soknader shouldHaveSize 1
 
-        val soknad = hentSoknader("87654321234").first()
-        soknad.sporsmal!!.flatten().first {
+        soknader.first().sporsmal!!.flatten().first {
             it.tag == "INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT"
         }.metadata.toString() `should be equal to` "null"
-
-        soknad.sporsmal!!.flatten().first {
-            it.tag == "INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET"
-        }.sporsmalstekst `should be equal to` "Er du ny i arbeidslivet etter 1. januar 2019?"
     }
 
-    @Disabled
     @Test
-    fun `metadata på spørsmål i kafka`() {
+    fun `Metadata med inntektsopplysniner fra Sigrun blir med på Kafka når søknaden sendes `() {
         fakeUnleash.enable("sykepengesoknad-backend-sigrun")
         val soknader =
             sendSykmelding(
@@ -102,35 +88,50 @@ class SigrunToggleTest : FellesTestOppsett() {
 
         soknader shouldHaveSize 1
 
-        val soknad = hentSoknader("87654321234").first()
         val sendtSoknad =
-            SoknadBesvarer(rSSykepengesoknad = soknad, mockMvc = this, fnr = "87654321234")
-                .besvarSporsmal(tag = ANSVARSERKLARING, svar = "CHECKED")
-                .besvarSporsmal(tag = medIndex(ARBEID_UNDERVEIS_100_PROSENT, 0), svar = "NEI")
-                .besvarSporsmal(tag = TILBAKE_I_ARBEID, svar = "NEI")
-                .besvarSporsmal(tag = ANDRE_INNTEKTSKILDER, svar = "NEI")
-                .besvarSporsmal(tag = OPPHOLD_UTENFOR_EOS, svar = "NEI")
-                .besvarSporsmal(tag = ARBEID_UTENFOR_NORGE, svar = "NEI")
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VIRKSOMHETEN_AVVIKLET, svar = null, ferdigBesvart = false)
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VIRKSOMHETEN_AVVIKLET_NEI, svar = "CHECKED", ferdigBesvart = false)
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET, svar = null, ferdigBesvart = false)
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_NEI, svar = "CHECKED", ferdigBesvart = false)
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING, svar = "JA", ferdigBesvart = false)
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_BEGRUNNELSE, svar = null, ferdigBesvart = false)
-                .besvarSporsmal(
-                    tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_BEGRUNNELSE_ENDRET_INNSATS,
-                    svar = "CHECKED",
-                    ferdigBesvart = false,
-                )
-                .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT, svar = "NEI")
-                .oppsummering()
+            SoknadBesvarer(rSSykepengesoknad = hentSoknader("87654321234").first(), mockMvc = this, fnr = "87654321234")
+                .besvarSporsmal()
                 .sendSoknad()
         sendtSoknad.status `should be equal to` RSSoknadstatus.SENDT
 
         juridiskVurderingKafkaConsumer.ventPåRecords(antall = 1)
         val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
-        kafkaSoknader.shouldHaveSize(1)
-        kafkaSoknader[0].sporsmal!!.flatten().first { it.tag == INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT }
+        kafkaSoknader.first().sporsmal!!.flatten().first { it.tag == INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT }
             .metadata `should not be` null
+    }
+
+    private fun SoknadBesvarer.besvarSporsmal(): SoknadBesvarer {
+        this.besvarSporsmal(tag = ANSVARSERKLARING, svar = "CHECKED")
+            .besvarSporsmal(tag = medIndex(ARBEID_UNDERVEIS_100_PROSENT, 0), svar = "NEI")
+            .besvarSporsmal(tag = TILBAKE_I_ARBEID, svar = "NEI")
+            .besvarSporsmal(tag = ANDRE_INNTEKTSKILDER, svar = "NEI")
+            .besvarSporsmal(tag = OPPHOLD_UTENFOR_EOS, svar = "NEI")
+            .besvarSporsmal(tag = ARBEID_UTENFOR_NORGE, svar = "NEI")
+            .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VIRKSOMHETEN_AVVIKLET, svar = null, ferdigBesvart = false)
+            .besvarSporsmal(
+                tag = INNTEKTSOPPLYSNINGER_VIRKSOMHETEN_AVVIKLET_NEI,
+                svar = "CHECKED",
+                ferdigBesvart = false,
+            )
+            .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET, svar = null, ferdigBesvart = false)
+            .besvarSporsmal(
+                tag = INNTEKTSOPPLYSNINGER_NY_I_ARBEIDSLIVET_NEI,
+                svar = "CHECKED",
+                ferdigBesvart = false,
+            )
+            .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING, svar = "JA", ferdigBesvart = false)
+            .besvarSporsmal(
+                tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_BEGRUNNELSE,
+                svar = null,
+                ferdigBesvart = false,
+            )
+            .besvarSporsmal(
+                tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_BEGRUNNELSE_ENDRET_INNSATS,
+                svar = "CHECKED",
+                ferdigBesvart = false,
+            )
+            .besvarSporsmal(tag = INNTEKTSOPPLYSNINGER_VARIG_ENDRING_25_PROSENT, svar = "NEI")
+            .oppsummering()
+        return this
     }
 }
