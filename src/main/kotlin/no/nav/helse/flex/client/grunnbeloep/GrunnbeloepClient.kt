8645e13ee36b286297201083c264c8ed43ae7ccf
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-12-21 15:03:32 +0100

    Bruk RestClient til henting av Grunnbeløp

--- src/main/kotlin/no/nav/helse/flex/client/grunnbeloep/GrunnbeloepClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/grunnbeloep/GrunnbeloepClient.kt
@@ -1,58 +1,40 @@
 package no.nav.helse.flex.client.grunnbeloep
 
-import no.nav.helse.flex.logger
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.helse.flex.util.objectMapper
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Mono
-import reactor.util.retry.Retry
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.RestClient
 import java.io.Serializable
-import java.time.Duration
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Component
 class GrunnbeloepClient(
-    webClientBuilder: WebClient.Builder,
-    @Value("\${GRUNNBELOEP_API_URL}") private val url: String,
+    @Value("\${GRUNNBELOEP_API_URL}")
+    private val url: String,
+    private val restClient: RestClient,
 ) {
-    val log = logger()
+    @Retryable(
+        value = [HttpServerErrorException::class, HttpClientErrorException::class],
+        maxAttempts = 3,
+        backoff = Backoff(delay = 1000),
+    )
+    fun hentGrunnbeloepHistorikk(hentForDato: LocalDate): List<GrunnbeloepResponse> {
+        return restClient.get()
+            .uri("$url/historikk/grunnbeløp?fra=$hentForDato")
+            .retrieve()
+            .body(String::class.java)!!.tilGrunnbeloepHistorikk()
+    }
 
-    // TODO: Bytt ut med RestTemplate (eventuelt RestClient) og fjern bruk av WebFlux.
-    private val webClient = webClientBuilder.baseUrl(url).build()
-
-    fun hentGrunnbeloepHistorikk(hentForDato: LocalDate): Mono<List<GrunnbeloepResponse>> {
-        val historikk =
-            webClient.get()
-                .uri { uriBuilder ->
-                    val formatertDato = hentForDato.format(DateTimeFormatter.ISO_DATE)
-                    uriBuilder.path("/historikk/grunnbeløp")
-                        .queryParam("fra", formatertDato)
-                        .build()
-                }
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .retrieve()
-                .onStatus({ status -> status.is4xxClientError || status.is5xxServerError }) { response ->
-                    response.createException()
-                        .flatMap { Mono.error(RuntimeException("Feil ved henting av grunnbeløphistorikk: ${response.statusCode()}")) }
-                }
-                .bodyToFlux(GrunnbeloepResponse::class.java)
-                .collectList()
-                .retryWhen(Retry.backoff(3, Duration.ofSeconds(2)))
-                .onErrorResume { error ->
-                    log.error("Feil ved henting av grunnbeløphistorikk fra $hentForDato til i dag", error)
-                    Mono.just(emptyList<GrunnbeloepResponse>())
-                }
-
-        // TODO: Fjern når vi har kontroll på hvor mange kall vi gjør per dag.
-        log.info("Hentet grunnbeløphistorikk fra $hentForDato til i dag.")
-        return historikk
+    private fun String.tilGrunnbeloepHistorikk(): List<GrunnbeloepResponse> {
+        return objectMapper.readValue(this)
     }
 }
 
-// TODO: Trenger ikke grunnbeløpPerMaaned, omregningsfaktor og virkningstidspunktForMinsteinntekt når vi bruker egen objectMapper.
 data class GrunnbeloepResponse(
     val dato: String,
     val grunnbeløp: Int,
