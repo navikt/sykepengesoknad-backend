f9322deb82cd4b5925d9a085a2a812bb22545b3f
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-01-13 12:34:42 +0100

    Slett sporsmal og svar fra allerede deaktiverte soknader

--- src/main/kotlin/no/nav/helse/flex/cronjob/DeaktiveringJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/DeaktiveringJob.kt
@@ -5,15 +5,24 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
+private const val ANTALL_SOKNADER = 100
+
 @Component
 class DeaktiveringJob(
     val deaktiverGamleSoknaderService: DeaktiverGamleSoknaderService,
     val leaderElection: LeaderElection,
 ) {
-    @Scheduled(initialDelay = 5, fixedDelay = 30, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(initialDelay = 5, fixedDelay = 60, timeUnit = TimeUnit.MINUTES)
     fun deaktiveringJob() {
         if (leaderElection.isLeader()) {
             deaktiverGamleSoknaderService.deaktiverSoknader()
+        }
+    }
+
+    @Scheduled(initialDelay = 180, fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
+    fun slettFraDeaktiverteSoknaderJob() {
+        if (leaderElection.isLeader()) {
+            deaktiverGamleSoknaderService.slettSporsmalOgSvarFraDeaktiverteSoknader(ANTALL_SOKNADER)
         }
     }
 }
