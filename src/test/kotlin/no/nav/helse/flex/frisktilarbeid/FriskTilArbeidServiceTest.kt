3db288c50e19e7e179a04c7dd6904b9c67564f05
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-01-24 16:12:50 +0100

    Fake @Repository med Spring

--- src/test/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidServiceTest.kt
+++ src/test/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidServiceTest.kt
@@ -3,14 +3,20 @@ package no.nav.helse.flex.frisktilarbeid
 import org.amshove.kluent.shouldHaveSize
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 import java.util.Optional
 import java.util.UUID
 
-
+@SpringBootTest(classes = [FriskTilArbeidService::class, FriskTilArbeidTestConfig::class])
 class FriskTilArbeidServiceTest {
+    @Autowired
+    private lateinit var friskTilArbeidService: FriskTilArbeidService
 
-    private val friskTilArbeidRepository = FakeMessageRepository()
-    private val friskTilArbeidService = FriskTilArbeidService(friskTilArbeidRepository)
+    @Autowired
+    private lateinit var friskTilArbeidRepository: FriskTilArbeidRepository
 
     private val fnr = "11111111111"
 
@@ -27,7 +33,7 @@ class FriskTilArbeidServiceTest {
             FriskTilArbeidVedtakStatusMelding(
                 key = key,
                 friskTilArbeidVedtakStatus = lagFriskTilArbeidVedtakStatus(fnr, Status.FATTET),
-            )
+            ),
         )
 
         friskTilArbeidRepository.findAll().toList() shouldHaveSize 1
@@ -41,67 +47,71 @@ class FriskTilArbeidServiceTest {
             FriskTilArbeidVedtakStatusMelding(
                 key = key,
                 friskTilArbeidVedtakStatus = lagFriskTilArbeidVedtakStatus(fnr, Status.FERDIG_BEHANDLET),
-            )
+            ),
         )
 
         friskTilArbeidRepository.findAll().toList() shouldHaveSize 0
     }
 }
 
-
 @Suppress("UNCHECKED_CAST")
-class FakeMessageRepository : FriskTilArbeidRepository {
+@TestConfiguration
+class FriskTilArbeidTestConfig {
+    @Bean
+    fun fakeFriskTilArbeidRepository(): FriskTilArbeidRepository {
+        return object : FriskTilArbeidRepository {
+            private val dbRecords = mutableMapOf<String, FriskTilArbeidDbRecord>()
 
-    private val dbRecords = mutableMapOf<String, FriskTilArbeidDbRecord>()
+            override fun <S : FriskTilArbeidDbRecord?> save(friskTilArbeidDbRecord: S & Any): S & Any {
+                val id = UUID.randomUUID().toString()
+                val lagretDbRecord = friskTilArbeidDbRecord.copy(id = id)
+                dbRecords[id] = lagretDbRecord
+                return lagretDbRecord as (S & Any)
+            }
 
-    override fun <S : FriskTilArbeidDbRecord?> save(friskTilArbeidDbRecord: S & Any): S & Any {
-        val id = UUID.randomUUID().toString()
-        val lagretDbRecord = friskTilArbeidDbRecord.copy(id = id)
-        dbRecords[id] = lagretDbRecord
-        return lagretDbRecord as (S & Any)
-    }
+            override fun deleteAll() {
+                dbRecords.clear()
+            }
 
-    override fun deleteAll() {
-        dbRecords.clear()
-    }
+            override fun findAll(): Iterable<FriskTilArbeidDbRecord?> {
+                return dbRecords.values
+            }
 
-    override fun findAll(): Iterable<FriskTilArbeidDbRecord?> {
-        return dbRecords.values
-    }
+            override fun <S : FriskTilArbeidDbRecord?> saveAll(entities: Iterable<S?>): Iterable<S?> {
+                TODO("Not yet implemented")
+            }
 
-    override fun <S : FriskTilArbeidDbRecord?> saveAll(entities: Iterable<S?>): Iterable<S?> {
-        TODO("Not yet implemented")
-    }
+            override fun findById(id: String): Optional<FriskTilArbeidDbRecord?> {
+                TODO("Not yet implemented")
+            }
 
-    override fun findById(id: String): Optional<FriskTilArbeidDbRecord?> {
-        TODO("Not yet implemented")
-    }
+            override fun existsById(id: String): Boolean {
+                TODO("Not yet implemented")
+            }
 
-    override fun existsById(id: String): Boolean {
-        TODO("Not yet implemented")
-    }
+            override fun findAllById(ids: Iterable<String?>): Iterable<FriskTilArbeidDbRecord?> {
+                TODO("Not yet implemented")
+            }
 
-    override fun findAllById(ids: Iterable<String?>): Iterable<FriskTilArbeidDbRecord?> {
-        TODO("Not yet implemented")
-    }
+            override fun count(): Long {
+                TODO("Not yet implemented")
+            }
 
-    override fun count(): Long {
-        TODO("Not yet implemented")
-    }
+            override fun deleteById(id: String) {
+                TODO("Not yet implemented")
+            }
 
-    override fun deleteById(id: String) {
-        TODO("Not yet implemented")
-    }
+            override fun delete(entity: FriskTilArbeidDbRecord) {
+                TODO("Not yet implemented")
+            }
 
-    override fun delete(entity: FriskTilArbeidDbRecord) {
-        TODO("Not yet implemented")
-    }
+            override fun deleteAllById(ids: Iterable<String?>) {
+                TODO("Not yet implemented")
+            }
 
-    override fun deleteAllById(ids: Iterable<String?>) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteAll(entities: Iterable<FriskTilArbeidDbRecord?>) {
-        TODO("Not yet implemented")
+            override fun deleteAll(entities: Iterable<FriskTilArbeidDbRecord?>) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
