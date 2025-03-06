8cce21c54af9cc5dcbb208ec5e5082d712cc6639
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2025-03-06 08:49:28 +0100

    Fjerner consumer seek awaer (#1010)

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
@@ -5,18 +5,14 @@ import no.nav.helse.flex.kafka.SIS_TOPIC
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.util.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
-import java.time.ZoneId
 
 @Component
 class VedtaksperiodeBehandlingConsumer(
     private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
-) : ConsumerSeekAware {
+) {
     val log = logger()
 
     @KafkaListener(
@@ -37,16 +33,5 @@ class VedtaksperiodeBehandlingConsumer(
         }
 
         acknowledgment.acknowledge()
-    }
-
-    override fun onPartitionsAssigned(
-        partitions: Map<TopicPartition, Long>,
-        callback: ConsumerSeekAware.ConsumerSeekCallback,
-    ) {
-        val desiredDate = LocalDateTime.of(2025, 2, 16, 0, 0)
-        val timestamp = desiredDate.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
-
-        log.info("Søker til $desiredDate for topic $SIS_TOPIC")
-        callback.seekToTimestamp(partitions.keys, timestamp)
     }
 }
