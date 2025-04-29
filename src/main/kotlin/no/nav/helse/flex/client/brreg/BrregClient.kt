92ebf01649f65a63ad1d0e6bce63782fd2582e9d
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-04-29 11:10:45 +0200

    Legg til håndtering av 404-feil i BrregClient og opprett tester for feilhåndtering

--- src/main/kotlin/no/nav/helse/flex/client/brreg/BrregClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/brreg/BrregClient.kt
@@ -1,7 +1,10 @@
 package no.nav.helse.flex.client.brreg
 
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.toEntity
@@ -17,11 +20,19 @@ class BrregClient(
     ): List<RolleDto> {
         val uri = brregRestClient.post().uri { uriBuilder -> uriBuilder.path("/api/v1/roller").build() }
         val hentRollerRequest = HentRollerRequest(fnr = fnr, rolleTyper = rolleTyper)
-        return uri
-            .body(hentRollerRequest)
-            .retrieve()
-            .toEntity<RollerDto>()
-            .body
-            ?.roller ?: emptyList()
+
+        return try {
+            uri
+                .headers {
+                    it.contentType = MediaType.APPLICATION_JSON
+                }
+                .body(hentRollerRequest)
+                .retrieve()
+                .toEntity<RollerDto>()
+                .body
+                ?.roller ?: emptyList()
+        } catch (_: HttpClientErrorException.NotFound) {
+            emptyList()
+        }
     }
 }
