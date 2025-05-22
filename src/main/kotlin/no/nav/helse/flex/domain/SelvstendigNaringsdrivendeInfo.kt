8593b3225bee8cebdc2e85fc0b957fcc9715c203
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-05-22 13:18:27 +0200

    Slett ubrukt SykepengegrunnlagNaeringsdrivendeDTO (#1119)

--- src/main/kotlin/no/nav/helse/flex/domain/SelvstendigNaringsdrivendeInfo.kt
+++ src/main/kotlin/no/nav/helse/flex/domain/SelvstendigNaringsdrivendeInfo.kt
@@ -13,45 +13,12 @@ data class SelvstendigNaringsdrivendeInfo(
         if (grunnlag == null) {
             return SelvstendigNaringsdrivendeDTO(
                 roller = roller.map { RolleDTO(it.orgnummer, it.rolletype) },
-                sykepengegrunnlagNaeringsdrivende = null,
                 naringsdrivendeInntekt = null,
             )
         }
 
         return SelvstendigNaringsdrivendeDTO(
             roller = roller.map { RolleDTO(it.orgnummer, it.rolletype) },
-            sykepengegrunnlagNaeringsdrivende =
-                SykepengegrunnlagNaeringsdrivendeDTO(
-                    gjennomsnittPerAar = grunnlag.gjennomsnittPerAar.map { AarVerdiDTO(it.aar, it.verdi) },
-                    grunnbeloepPerAar = grunnlag.grunnbeloepPerAar.map { AarVerdiDTO(it.aar, it.verdi) },
-                    grunnbeloepPaaSykmeldingstidspunkt = grunnlag.grunnbeloepPaaSykmeldingstidspunkt,
-                    beregnetSnittOgEndring25 =
-                        BeregnetDTO(
-                            snitt = grunnlag.beregnetSnittOgEndring25.snitt,
-                            p25 = grunnlag.beregnetSnittOgEndring25.p25,
-                            m25 = grunnlag.beregnetSnittOgEndring25.m25,
-                        ),
-                    inntekter =
-                        grunnlag.inntekter.map { hentPensjonsgivendeInntektResponse ->
-                            HentPensjonsgivendeInntektResponseDTO(
-                                norskPersonidentifikator = hentPensjonsgivendeInntektResponse.norskPersonidentifikator,
-                                inntektsaar = hentPensjonsgivendeInntektResponse.inntektsaar,
-                                pensjonsgivendeInntekt =
-                                    hentPensjonsgivendeInntektResponse.pensjonsgivendeInntekt.map {
-                                        PensjonsgivendeInntektDTO(
-                                            datoForFastsetting = it.datoForFastsetting,
-                                            skatteordning = SkatteordningDTO.valueOf(it.skatteordning.name),
-                                            pensjonsgivendeInntektAvLoennsinntekt = it.pensjonsgivendeInntektAvLoennsinntekt,
-                                            pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel =
-                                                it.pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel,
-                                            pensjonsgivendeInntektAvNaeringsinntekt = it.pensjonsgivendeInntektAvNaeringsinntekt,
-                                            pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage =
-                                                it.pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage,
-                                        )
-                                    },
-                            )
-                        },
-                ),
             naringsdrivendeInntekt =
                 NaringsdrivendeInntektDTO(
                     norskPersonidentifikator = sykepengegrunnlagNaeringsdrivende.inntekter.first().norskPersonidentifikator,
@@ -60,7 +27,7 @@ data class SelvstendigNaringsdrivendeInfo(
         )
     }
 
-    fun mapToNaringsdrivendeInntektsAarDTO(): List<NaringsdrivendeInntektsAarDTO> =
+    private fun mapToNaringsdrivendeInntektsAarDTO(): List<NaringsdrivendeInntektsAarDTO> =
         sykepengegrunnlagNaeringsdrivende!!.inntekter.map { inntekt ->
             NaringsdrivendeInntektsAarDTO(
                 inntektsaar = inntekt.inntektsaar,
--- src/test/kotlin/no/nav/helse/flex/domain/mapper/SelvstendigNaringsdrivendeToSykepengesoknadDtoTest.kt
+++ src/test/kotlin/no/nav/helse/flex/domain/mapper/SelvstendigNaringsdrivendeToSykepengesoknadDtoTest.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.flex.domain.mapper
 
-import no.nav.helse.flex.domain.BrregRolle
 import no.nav.helse.flex.domain.Mottaker
 import no.nav.helse.flex.domain.SelvstendigNaringsdrivendeInfo
 import no.nav.helse.flex.domain.Soknadsperiode
@@ -8,10 +7,9 @@ import no.nav.helse.flex.domain.Sykepengesoknad
 import no.nav.helse.flex.domain.mapper.sporsmalprossesering.hentSoknadsPerioderMedFaktiskGrad
 import no.nav.helse.flex.mock.opprettNyNaeringsdrivendeSoknad
 import no.nav.helse.flex.soknadsopprettelse.lagSykepengegrunnlagNaeringsdrivende
-import no.nav.helse.flex.sykepengesoknad.kafka.*
+import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
-import java.math.BigInteger
 import java.time.LocalDate
 
 @Suppress("DEPRECATION")
@@ -26,57 +24,6 @@ class SelvstendigNaringsdrivendeToSykepengesoknadDtoTest {
                 sykmeldingstype = null,
             ),
         )
-
-    @Test
-    fun `Inneholder roller i selvstendigNaringsdrivende`() {
-        val soknad =
-            opprettNyNaeringsdrivendeSoknad().copy(
-                soknadPerioder = soknadPerioder,
-                selvstendigNaringsdrivende =
-                    SelvstendigNaringsdrivendeInfo(
-                        listOf(
-                            BrregRolle(
-                                "123456789",
-                                "Test",
-                                "ROLLE",
-                            ),
-                        ),
-                    ),
-            )
-        val soknadDto =
-            lagSykepengesoknadDTO(soknad)
-
-        soknadDto.selvstendigNaringsdrivende?.let {
-            it.roller `should be equal to`
-                listOf(
-                    RolleDTO(
-                        "123456789",
-                        "ROLLE",
-                    ),
-                )
-            it.sykepengegrunnlagNaeringsdrivende `should be equal to` null
-        }
-    }
-
-    @Test
-    fun `Inneholder sykepengegrunnlag i selvstedigNaringsdrivende`() {
-        val soknad =
-            opprettNyNaeringsdrivendeSoknad().copy(
-                soknadPerioder = soknadPerioder,
-                selvstendigNaringsdrivende =
-                    SelvstendigNaringsdrivendeInfo(
-                        roller = emptyList(),
-                        sykepengegrunnlagNaeringsdrivende = lagSykepengegrunnlagNaeringsdrivende(),
-                    ),
-            )
-
-        val soknadDto = lagSykepengesoknadDTO(soknad)
-
-        soknadDto.selvstendigNaringsdrivende?.let {
-            it.roller `should be equal to` emptyList()
-            it.sykepengegrunnlagNaeringsdrivende `should be equal to` sykepengegrunnlagNaeringsdrivendeDTO
-        }
-    }
 
     @Test
     fun `Inneholder summert inntektsinformasjon i selvstendigNaringsdrivende`() {
@@ -127,90 +74,3 @@ class SelvstendigNaringsdrivendeToSykepengesoknadDtoTest {
             soknadsperioder = hentSoknadsPerioderMedFaktiskGrad(soknad).first,
         )
 }
-
-private val sykepengegrunnlagNaeringsdrivendeDTO =
-    SykepengegrunnlagNaeringsdrivendeDTO(
-        gjennomsnittPerAar =
-            listOf(
-                AarVerdiDTO(aar = "2023", verdi = BigInteger("851782")),
-                AarVerdiDTO(aar = "2022", verdi = BigInteger("872694")),
-                AarVerdiDTO(aar = "2021", verdi = BigInteger("890920")),
-            ),
-        grunnbeloepPerAar =
-            listOf(
-                AarVerdiDTO(aar = "2021", verdi = BigInteger("104716")),
-                AarVerdiDTO(aar = "2022", verdi = BigInteger("109784")),
-                AarVerdiDTO(aar = "2023", verdi = BigInteger("116239")),
-            ),
-        grunnbeloepPaaSykmeldingstidspunkt = 124028,
-        beregnetSnittOgEndring25 =
-            BeregnetDTO(
-                snitt = BigInteger("871798"),
-                p25 = BigInteger("1089748"),
-                m25 = BigInteger("653849"),
-            ),
-        inntekter =
-            listOf(
-                HentPensjonsgivendeInntektResponseDTO(
-                    norskPersonidentifikator = "123456789",
-                    inntektsaar = "2023",
-                    pensjonsgivendeInntekt =
-                        listOf(
-                            PensjonsgivendeInntektDTO(
-                                datoForFastsetting = LocalDate.parse("2023-07-17").toString(),
-                                skatteordning = SkatteordningDTO.FASTLAND,
-                                pensjonsgivendeInntektAvLoennsinntekt = 100_000,
-                                pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel = 50_000,
-                                pensjonsgivendeInntektAvNaeringsinntekt = 300_000,
-                                pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage = 50_000,
-                            ),
-                            PensjonsgivendeInntektDTO(
-                                datoForFastsetting = LocalDate.parse("2023-07-17").toString(),
-                                skatteordning = SkatteordningDTO.SVALBARD,
-                                pensjonsgivendeInntektAvLoennsinntekt = 100_000,
-                                pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel = 50_000,
-                                pensjonsgivendeInntektAvNaeringsinntekt = 300_000,
-                                pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage = 50_000,
-                            ),
-                        ),
-                ),
-                HentPensjonsgivendeInntektResponseDTO(
-                    norskPersonidentifikator = "123456789",
-                    inntektsaar = "2022",
-                    pensjonsgivendeInntekt =
-                        listOf(
-                            PensjonsgivendeInntektDTO(
-                                datoForFastsetting = LocalDate.parse("2022-07-17").toString(),
-                                skatteordning = SkatteordningDTO.FASTLAND,
-                                pensjonsgivendeInntektAvLoennsinntekt = 50_000,
-                                pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel = 50_000,
-                                pensjonsgivendeInntektAvNaeringsinntekt = 350_000,
-                                pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage = 50_000,
-                            ),
-                            PensjonsgivendeInntektDTO(
-                                datoForFastsetting = LocalDate.parse("2022-07-17").toString(),
-                                skatteordning = SkatteordningDTO.SVALBARD,
-                                pensjonsgivendeInntektAvLoennsinntekt = 50_000,
-                                pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel = 50_000,
-                                pensjonsgivendeInntektAvNaeringsinntekt = 350_000,
-                                pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage = 50_000,
-                            ),
-                        ),
-                ),
-                HentPensjonsgivendeInntektResponseDTO(
-                    norskPersonidentifikator = "123456789",
-                    inntektsaar = "2021",
-                    pensjonsgivendeInntekt =
-                        listOf(
-                            PensjonsgivendeInntektDTO(
-                                datoForFastsetting = LocalDate.parse("2021-07-17").toString(),
-                                skatteordning = SkatteordningDTO.FASTLAND,
-                                pensjonsgivendeInntektAvLoennsinntekt = 10_000,
-                                pensjonsgivendeInntektAvLoennsinntektBarePensjonsdel = 190_000,
-                                pensjonsgivendeInntektAvNaeringsinntekt = 500_000,
-                                pensjonsgivendeInntektAvNaeringsinntektFraFiskeFangstEllerFamiliebarnehage = 300_000,
-                            ),
-                        ),
-                ),
-            ),
-    )
