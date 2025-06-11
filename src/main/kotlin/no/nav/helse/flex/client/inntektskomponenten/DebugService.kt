49b758bec301ee46d8d33482b1c03f838c051c77
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-06-11 11:42:32 +0200

    Logg inntekt as JSON

--- src/main/kotlin/no/nav/helse/flex/client/inntektskomponenten/DebugService.kt
+++ src/main/kotlin/no/nav/helse/flex/client/inntektskomponenten/DebugService.kt
@@ -1,7 +1,9 @@
 package no.nav.helse.flex.client.inntektskomponenten
 
+import no.nav.helse.flex.cronjob.LeaderElection
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.repository.SykepengesoknadRepository
+import no.nav.helse.flex.util.serialisertTilString
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.YearMonth
@@ -11,27 +13,19 @@ import java.util.concurrent.TimeUnit
 class DebugService(
     private val inntektskomponentenClient: InntektskomponentenClient,
     private val sykepengesoknadRepository: SykepengesoknadRepository,
+    private val leaderElection: LeaderElection,
 ) {
     private val log = logger()
 
     @Scheduled(initialDelay = 3, fixedDelay = 3600, timeUnit = TimeUnit.MINUTES)
     fun debugInntekt() {
-        val fnr = sykepengesoknadRepository.findBySykepengesoknadUuid("9150a8a7-20f6-31e8-9def-a4123665ba95")!!.fnr
-        val fom = YearMonth.of(2025, 2)
-        val tom = YearMonth.of(2025, 5)
-        val inntekter = inntektskomponentenClient.hentInntekter(fnr, fom, tom)
+        if (!leaderElection.isLeader()) {
+            val fnr = sykepengesoknadRepository.findBySykepengesoknadUuid("9150a8a7-20f6-31e8-9def-a4123665ba95")!!.fnr
+            val fom = YearMonth.of(2025, 2)
+            val tom = YearMonth.of(2025, 5)
+            val inntekter = inntektskomponentenClient.hentInntekter(fnr, fom, tom)
 
-        inntekter.arbeidsInntektMaaned.forEach {
-            val inntektListe = it.arbeidsInntektInformasjon.inntektListe
-            val arbeidsforholdListe = it.arbeidsInntektInformasjon.arbeidsforholdListe
-
-            inntektListe.forEach {
-                log.info("Hentet inntekter.aktoerType ${it.virksomhet.aktoerType}")
-            }
-
-            arbeidsforholdListe.forEach {
-                log.info("Hentet arbeidsgiver.aktoerType ${it.arbeidsgiver.aktoerType}")
-            }
+            log.info("Hentet inntekter for soknad: 9150a8a7-20f6-31e8-9def-a4123665ba95: ${inntekter.serialisertTilString()}")
         }
     }
 }
