70d45cb214e7f61fdcf5f800d441e00f07a526c6
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-07-07 15:22:30 +0200

    Legg til endepunkt for sletting av FriskTilArbeid-søknader

--- src/test/kotlin/no/nav/helse/flex/controller/FlexAPITest.kt
+++ src/test/kotlin/no/nav/helse/flex/controller/FlexAPITest.kt
@@ -1,33 +1,55 @@
 package no.nav.helse.flex.controller
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import no.nav.helse.flex.*
+import no.nav.helse.flex.FellesTestOppsett
 import no.nav.helse.flex.client.aareg.Arbeidsforhold
 import no.nav.helse.flex.client.arbeidssokerregister.ArbeidssokerperiodeResponse
 import no.nav.helse.flex.client.pdl.PdlIdent
 import no.nav.helse.flex.client.sigrun.HentPensjonsgivendeInntektResponse
 import no.nav.helse.flex.domain.AuditEntry
 import no.nav.helse.flex.domain.EventType
+import no.nav.helse.flex.domain.Soknadstatus
+import no.nav.helse.flex.domain.Sykepengesoknad
+import no.nav.helse.flex.frisktilarbeid.FriskTilArbeidRepository
+import no.nav.helse.flex.frisktilarbeid.FriskTilArbeidVedtakStatus
+import no.nav.helse.flex.frisktilarbeid.Status
+import no.nav.helse.flex.frisktilarbeid.lagFriskTilArbeidSoknad
+import no.nav.helse.flex.frisktilarbeid.lagFriskTilArbeidVedtakDbRecord
+import no.nav.helse.flex.frisktilarbeid.lagFriskTilArbeidVedtakStatus
+import no.nav.helse.flex.jwt
+import no.nav.helse.flex.repository.SykepengesoknadDAO
+import no.nav.helse.flex.sendSykmelding
+import no.nav.helse.flex.skapAzureJwt
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import no.nav.helse.flex.testdata.heltSykmeldt
 import no.nav.helse.flex.testdata.sykmeldingKafkaMessage
 import no.nav.helse.flex.util.objectMapper
 import no.nav.helse.flex.util.serialisertTilString
+import no.nav.helse.flex.ventPåRecords
 import org.amshove.kluent.`should be`
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldHaveSize
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.net.URI
+import java.time.LocalDate
 
 class FlexAPITest : FellesTestOppsett() {
     val fnr = "12345678901"
     val fnrFlexer = "10987654321"
     lateinit var kafkaMelding: SykepengesoknadDTO
+
+    @Autowired
+    private lateinit var sykepengesoknadDAO: SykepengesoknadDAO
+
+    @Autowired
+    private lateinit var friskTilArbeidRepository: FriskTilArbeidRepository
 
     @BeforeAll
     fun `Legger til søknader i databasen`() {
@@ -47,8 +69,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/sykepengesoknader")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.HentSykepengesoknaderRequest(fnr).serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.HentSykepengesoknaderRequest(fnr).serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -108,8 +132,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/sykepengesoknader")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.HentSykepengesoknaderRequest(fnr).serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.HentSykepengesoknaderRequest(fnr).serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -166,8 +192,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/identer")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.HentIdenterRequest("211111111111").serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.HentIdenterRequest("211111111111").serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -200,8 +228,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/aareg")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.HentAaregdataRequest("22222220001").serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.HentAaregdataRequest("22222220001").serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -225,15 +255,20 @@ class FlexAPITest : FellesTestOppsett() {
     }
 
     @Test
-    fun `Kan hente sigrundata fra flex-internal-frontend`() {
+    fun `Kan hente Sigrun-data fra flex-internal-frontend`() {
         val result =
             mockMvc
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/sigrun")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.HentPensjonsgivendeInntektRequest("22222220001", "2024").serialisertTilString())
-                        .contentType(MediaType.APPLICATION_JSON),
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(
+                            SoknadFlexAzureController
+                                .HentPensjonsgivendeInntektRequest("22222220001", "2024")
+                                .serialisertTilString(),
+                        ).contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
 
@@ -262,8 +297,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/arbeidssokerregister")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.FnrRequest("22222220001").serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.FnrRequest("22222220001").serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -294,8 +331,10 @@ class FlexAPITest : FellesTestOppsett() {
                 .perform(
                     MockMvcRequestBuilders
                         .post("/api/v1/flex/fta-vedtak-for-person")
-                        .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
-                        .content(SoknadFlexAzureController.FnrRequest("22222220001").serialisertTilString())
+                        .header(
+                            "Authorization",
+                            "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}",
+                        ).content(SoknadFlexAzureController.FnrRequest("22222220001").serialisertTilString())
                         .contentType(MediaType.APPLICATION_JSON),
                 ).andExpect(MockMvcResultMatchers.status().isOk)
                 .andReturn()
@@ -327,5 +366,156 @@ class FlexAPITest : FellesTestOppsett() {
                     .content(SoknadFlexAzureController.HentIdenterRequest("211111111111").serialisertTilString())
                     .contentType(MediaType.APPLICATION_JSON),
             ).andExpect(MockMvcResultMatchers.status().is4xxClientError)
+    }
+
+    @Test
+    fun `Kan slette FriskTilArbeid-søknad med status NY`() {
+        val fnr = "11111111111"
+        val friskTilArbeidVedtakId = lagreFriskTilArbeidVedtakStatus(lagFriskTilArbeidVedtakStatus(fnr, Status.FATTET))
+
+        val sendtSoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.SENDT,
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(4),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        val nySoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.NY,
+                fom = LocalDate.now().plusDays(5),
+                tom = LocalDate.now().plusDays(9),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        sykepengesoknadDAO.lagreSykepengesoknad(nySoknad)
+
+        slettSoknad(fnr, nySoknad, friskTilArbeidVedtakId)
+
+        sykepengesoknadDAO.finnSykepengesoknader(listOf(fnr)).also {
+            it.single().id `should be equal to` sendtSoknad.id
+        }
+
+        verifiserAuditLog(fnr, nySoknad)
+    }
+
+    @Test
+    fun `Kan slette FriskTilArbeid-søknad med status FREMTIDIG`() {
+        val fnr = "22222222222"
+        val friskTilArbeidVedtakId = lagreFriskTilArbeidVedtakStatus(lagFriskTilArbeidVedtakStatus(fnr, Status.FATTET))
+
+        val sendtSoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.SENDT,
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(4),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        val fremtidigSoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.FREMTIDIG,
+                fom = LocalDate.now().plusDays(5),
+                tom = LocalDate.now().plusDays(9),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        sykepengesoknadDAO.lagreSykepengesoknad(fremtidigSoknad)
+
+        slettSoknad(fnr, fremtidigSoknad, friskTilArbeidVedtakId)
+
+        sykepengesoknadDAO.finnSykepengesoknader(listOf(fnr)).also {
+            it.single().id `should be equal to` sendtSoknad.id
+        }
+
+        verifiserAuditLog(fnr, fremtidigSoknad)
+    }
+
+    @Test
+    fun `Kan ikke slette FRISKMELDT_TIL_ARBEIDSFORMIDLING søknad med status SENDT`() {
+        val fnr = "33333333333"
+        val friskTilArbeidVedtakId = lagreFriskTilArbeidVedtakStatus(lagFriskTilArbeidVedtakStatus(fnr, Status.FATTET))
+
+        val sendtSoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.SENDT,
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(4),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        sykepengesoknadDAO.lagreSykepengesoknad(sendtSoknad)
+
+        val result = slettSoknad(fnr, sendtSoknad, friskTilArbeidVedtakId)
+
+        result.response.status `should be equal to` 400
+        result.response.contentAsString `should be equal to` "Kan kun slette søknader med status NY eller FREMTIDIG."
+    }
+
+    @Test
+    fun `Feiler når FriskTilArbeid-søknad ikke tilhører bruker`() {
+        val fnr = "55555555555"
+        val friskTilArbeidVedtakId = lagreFriskTilArbeidVedtakStatus(lagFriskTilArbeidVedtakStatus(fnr, Status.FATTET))
+
+        val nySoknad =
+            lagFriskTilArbeidSoknad(
+                fnr = fnr,
+                friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+                soknadStatus = Soknadstatus.NY,
+                fom = LocalDate.now(),
+                tom = LocalDate.now().plusDays(4),
+            ).also { sykepengesoknadDAO.lagreSykepengesoknad(it) }
+
+        sykepengesoknadDAO.lagreSykepengesoknad(nySoknad)
+
+        val result = slettSoknad("66666666666", nySoknad, friskTilArbeidVedtakId)
+
+        result.response.status `should be equal to` 404
+        result.response.contentAsString `should be equal to` "Fant ikke søknad med id: ${nySoknad.id} tilhørende den aktuelle brukeren."
+    }
+
+    private fun lagreFriskTilArbeidVedtakStatus(friskTilArbeidVedtakStatus: FriskTilArbeidVedtakStatus): String =
+        friskTilArbeidRepository.save(lagFriskTilArbeidVedtakDbRecord(friskTilArbeidVedtakStatus)).id!!
+
+    private fun slettSoknad(
+        fnr: String,
+        nySoknad: Sykepengesoknad,
+        friskTilArbeidVedtakId: String,
+    ): MvcResult =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders
+                    .delete("/api/v1/flex/fta-soknad")
+                    .header("Authorization", "Bearer ${skapAzureJwt("flex-internal-frontend-client-id", fnrFlexer)}")
+                    .content(
+                        SoknadFlexAzureController
+                            .SlettSykepengesoknadRequest(fnr, nySoknad.id)
+                            .serialisertTilString(),
+                    ).contentType(MediaType.APPLICATION_JSON),
+            ).andReturn()
+
+    private fun verifiserAuditLog(
+        fnr: String,
+        nySoknad: Sykepengesoknad,
+    ) {
+        auditlogKafkaConsumer.ventPåRecords(1).first().let {
+            val auditEntry: AuditEntry = objectMapper.readValue(it.value())
+            with(auditEntry) {
+                appNavn `should be equal to` "flex-internal"
+                utførtAv `should be equal to` fnrFlexer
+                oppslagPå `should be equal to` fnr
+                eventType `should be equal to` EventType.DELETE
+                forespørselTillatt `should be` true
+                beskrivelse `should be equal to` "Sletter sykepengesoknad av type FRISKMELDT_TIL_ARBEIDSFORMIDLING med id: ${nySoknad.id}."
+                requestUrl `should be equal to` URI.create("http://localhost/api/v1/flex/fta-soknad")
+                requestMethod `should be equal to` "DELETE"
+            }
+        }
     }
 }
--- src/test/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidTestUtils.kt
+++ src/test/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidTestUtils.kt
@@ -1,6 +1,11 @@
 package no.nav.helse.flex.frisktilarbeid
 
 import no.nav.helse.flex.domain.Periode
+import no.nav.helse.flex.domain.Soknadstatus
+import no.nav.helse.flex.domain.Soknadstype
+import no.nav.helse.flex.domain.Sykepengesoknad
+import no.nav.helse.flex.medlemskap.tilPostgresJson
+import java.time.Instant
 import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.*
@@ -22,3 +27,52 @@ internal fun lagFriskTilArbeidVedtakStatus(
         statusAt = OffsetDateTime.now(),
         statusBy = "Test",
     )
+
+internal fun lagFriskTilArbeidVedtakDbRecord(friskTilArbeidVedtak: FriskTilArbeidVedtakStatus): FriskTilArbeidVedtakDbRecord {
+    val friskTilArbeidVedtakDbRecord =
+        FriskTilArbeidVedtakDbRecord(
+            vedtakUuid = UUID.randomUUID().toString(),
+            key = friskTilArbeidVedtak.personident.asProducerRecordKey(),
+            opprettet = Instant.now(),
+            fnr = friskTilArbeidVedtak.personident,
+            fom = friskTilArbeidVedtak.fom,
+            tom = friskTilArbeidVedtak.tom,
+            vedtak = friskTilArbeidVedtak.tilPostgresJson(),
+            behandletStatus = BehandletStatus.BEHANDLET,
+            ignorerArbeidssokerregister = null,
+        )
+    return friskTilArbeidVedtakDbRecord
+}
+
+internal fun lagFriskTilArbeidSoknad(
+    fnr: String,
+    friskTilArbeidVedtakId: String,
+    soknadStatus: Soknadstatus,
+    fom: LocalDate,
+    tom: LocalDate,
+): Sykepengesoknad {
+    val soknad =
+        Sykepengesoknad(
+            id = UUID.randomUUID().toString(),
+            fnr = fnr,
+            soknadstype = Soknadstype.FRISKMELDT_TIL_ARBEIDSFORMIDLING,
+            status = soknadStatus,
+            opprettet = Instant.now(),
+            startSykeforlop = LocalDate.now(),
+            fom = fom,
+            tom = tom,
+            arbeidssituasjon = null,
+            arbeidsgiverOrgnummer = null,
+            arbeidsgiverNavn = null,
+            sykmeldingId = UUID.randomUUID().toString(),
+            sykmeldingSkrevet = null,
+            soknadPerioder = emptyList(),
+            egenmeldtSykmelding = null,
+            sporsmal = emptyList(),
+            utenlandskSykmelding = false,
+            egenmeldingsdagerFraSykmelding = null,
+            forstegangssoknad = false,
+            friskTilArbeidVedtakId = friskTilArbeidVedtakId,
+        )
+    return soknad
+}
