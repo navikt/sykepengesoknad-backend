4aff08dd880a02ed2bd12e018667e9021040694e
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-08-08 06:28:41 +0200

    Overser feilende spørsmål om OPPHOLDSTILLATELSE for søknader som må aktiveres (#1171)
    (cherry picked from commit 6807df6e7ebdd2650ea568c70d607c974202bc57)

---
+++ src/test/kotlin/no/nav/helse/flex/medlemskap/MedlemskapVurderingClientTest.kt
@@ -0,0 +1,109 @@
+package no.nav.helse.flex.medlemskap
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should contain same`
+import org.amshove.kluent.shouldNotContain
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
+import java.time.LocalDate
+import java.util.*
+
+class MedlemskapVurderingClientTest {
+    private val medlemskapVurderingRepository = mock<MedlemskapVurderingRepository>()
+    private val medlemskapVurderingRestTemplate = mock<RestTemplate>()
+    private val url = "http://test-url"
+
+    private val medlemskapVurderingClient =
+        MedlemskapVurderingClient(
+            medlemskapVurderingRepository,
+            medlemskapVurderingRestTemplate,
+            url,
+        )
+
+    @Test
+    fun `Fjerner spørsmål om OPPHOLDSTILLATELSE hvis kjentOppholdstillatelse mangler for angitt søknad`() {
+        val sykepengesoknadId = "b9b435b8-8c58-3595-980b-63a6d7c2465f"
+        val request =
+            MedlemskapVurderingRequest(
+                fnr = "12345678901",
+                fom = LocalDate.of(2023, 1, 1),
+                tom = LocalDate.of(2023, 1, 31),
+                sykepengesoknadId = sykepengesoknadId,
+            )
+
+        val mockResponse =
+            MedlemskapVurderingResponse(
+                svar = MedlemskapVurderingSvarType.UAVKLART,
+                sporsmal =
+                    listOf(
+                        MedlemskapVurderingSporsmal.OPPHOLDSTILATELSE,
+                        MedlemskapVurderingSporsmal.ARBEID_UTENFOR_NORGE,
+                    ),
+                kjentOppholdstillatelse = null,
+            )
+
+        whenever(
+            medlemskapVurderingRestTemplate.exchange(
+                any<String>(),
+                any<HttpMethod>(),
+                any<HttpEntity<*>>(),
+                any<Class<MedlemskapVurderingResponse>>(),
+            ),
+        ).thenReturn(ResponseEntity.ok(mockResponse))
+
+        medlemskapVurderingClient.hentMedlemskapVurdering(request).also {
+            it.svar `should be equal to` MedlemskapVurderingSvarType.UAVKLART
+            it.sporsmal shouldNotContain MedlemskapVurderingSporsmal.OPPHOLDSTILATELSE
+            it.sporsmal `should contain same` listOf(MedlemskapVurderingSporsmal.ARBEID_UTENFOR_NORGE)
+            it.kjentOppholdstillatelse `should be equal to` null
+        }
+
+        verify(medlemskapVurderingRepository).save(any())
+    }
+
+    @Test
+    fun `Kaster exception når spørsmål om OPPHOLDSTILLATELSE mangler kjentOppholdstillatelse`() {
+        val sykepengesoknadId = UUID.randomUUID().toString()
+        val request =
+            MedlemskapVurderingRequest(
+                fnr = "12345678901",
+                fom = LocalDate.of(2023, 1, 1),
+                tom = LocalDate.of(2023, 1, 31),
+                sykepengesoknadId = sykepengesoknadId,
+            )
+
+        val mockResponse =
+            MedlemskapVurderingResponse(
+                svar = MedlemskapVurderingSvarType.UAVKLART,
+                sporsmal =
+                    listOf(
+                        MedlemskapVurderingSporsmal.OPPHOLDSTILATELSE,
+                        MedlemskapVurderingSporsmal.ARBEID_UTENFOR_NORGE,
+                    ),
+                kjentOppholdstillatelse = null,
+            )
+
+        whenever(
+            medlemskapVurderingRestTemplate.exchange(
+                any<String>(),
+                any<HttpMethod>(),
+                any<HttpEntity<*>>(),
+                any<Class<MedlemskapVurderingResponse>>(),
+            ),
+        ).thenReturn(ResponseEntity.ok(mockResponse))
+
+        assertThrows<MedlemskapVurderingResponseException> {
+            medlemskapVurderingClient.hentMedlemskapVurdering(request)
+        }
+
+        verify(medlemskapVurderingRepository).save(any())
+    }
+}
