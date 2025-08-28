a147148175b29f1441a058727ae6a848335fbb5a
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-08-28 11:37:00 +0200

    wip fungerer ikke

--- src/main/kotlin/no/nav/helse/flex/client/ereg/EregClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/ereg/EregClient.kt
@@ -17,7 +17,7 @@ import org.springframework.web.util.UriComponentsBuilder
 class EregClient(
     private val plainRestTemplate: RestTemplate,
     private val environmentToggles: EnvironmentToggles,
-    @Value("\${EREG_URL}") private val eregUrl: String,
+    @Value("\${EREG_URL:http://dummy-url.com}") private val eregUrl: String,
 ) {
     val log = logger()
 
