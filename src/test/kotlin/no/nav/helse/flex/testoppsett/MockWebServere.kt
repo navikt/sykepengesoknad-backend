c029344892d28f8eb4c39105c13cb0e02568ccaa
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-08-29 14:22:07 +0200

    fjerner kommentar

--- src/test/kotlin/no/nav/helse/flex/testoppsett/MockWebServere.kt
+++ src/test/kotlin/no/nav/helse/flex/testoppsett/MockWebServere.kt
@@ -67,7 +67,6 @@ fun startMockWebServere(): MockWebServere {
     val enhetsregisterMockWebServer =
         MockWebServer().apply {
             System.setProperty("ENHETSREGISTER_BASE_URL", "http://localhost:$port")
-            // dispatcher = SimpleEnhetsregisterMockDispatcher
             dispatcher = EnhetsregisterMockDispatcher
         }
 
