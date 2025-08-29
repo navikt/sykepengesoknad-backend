f9d7d38156a1c283dbfacac9ed9bca6b28f657c8
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-08-29 10:59:48 +0200

    wip

--- src/test/kotlin/no/nav/helse/flex/mockdispatcher/EnhetsregisterMockDispatcher.kt
+++ src/test/kotlin/no/nav/helse/flex/mockdispatcher/EnhetsregisterMockDispatcher.kt
@@ -4,7 +4,7 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.QueueDispatcher
 import okhttp3.mockwebserver.RecordedRequest
 
-// brukes ikke nå, bør den fjernes? - eller brukes istedenfor den som virker?
+// brukes n bedre enn den enkle?å  bør den fjernes? - eller brukes istedenfor den som virker?
 object EnhetsregisterMockDispatcher : QueueDispatcher() {
     override fun dispatch(request: RecordedRequest): MockResponse {
         if (responseQueue.peek() != null) {
