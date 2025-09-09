7362cb17fdbc6eed9bff47421a3282cf3d27867d
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-09-09 17:39:10 +0200

    Bump sykepengesoknad-kafka til 2025.09.09-07.30-baf456bb

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ repositories {
 val tokenSupportVersion = "5.0.35"
 val confluentVersion = "8.0.0"
 val syfoKafkaVersion = "2021.07.20-09.39-6be2c52c"
-val sykepengesoknadKafkaVersion = "2025.09.01-12.25-565bb741"
+val sykepengesoknadKafkaVersion = "2025.09.09-07.30-baf456bb"
 val mockitoKotlinVersion = "2.2.0"
 val avroVersion = "1.12.0"
 val logstashLogbackEncoderVersion = "8.1"
