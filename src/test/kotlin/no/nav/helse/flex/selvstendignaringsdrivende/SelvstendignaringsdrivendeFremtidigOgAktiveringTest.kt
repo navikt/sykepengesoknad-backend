313904c8c2b68540f2ab5d031aa7f51263885c70
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-09-25 14:55:42 +0200

    førstegangssøknad er forksjellig fra nr 2

--- src/test/kotlin/no/nav/helse/flex/selvstendignaringsdrivende/SelvstendignaringsdrivendeFremtidigOgAktiveringTest.kt
+++ src/test/kotlin/no/nav/helse/flex/selvstendignaringsdrivende/SelvstendignaringsdrivendeFremtidigOgAktiveringTest.kt
@@ -2,21 +2,21 @@ package no.nav.helse.flex.selvstendignaringsdrivende
 
 import no.nav.helse.flex.*
 import no.nav.helse.flex.aktivering.AktiveringJob
+import no.nav.helse.flex.client.grunnbeloep.GrunnbeloepResponse
 import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstatus
 import no.nav.helse.flex.controller.domain.sykepengesoknad.RSSoknadstype
 import no.nav.helse.flex.domain.Arbeidssituasjon
+import no.nav.helse.flex.mockdispatcher.grunnbeloep.GrunnbeloepApiMockDispatcher
 import no.nav.helse.flex.soknadsopprettelse.*
 import no.nav.helse.flex.sykepengesoknad.kafka.ArbeidssituasjonDTO
 import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
 import no.nav.helse.flex.testdata.heltSykmeldt
 import no.nav.helse.flex.testdata.sykmeldingKafkaMessage
 import no.nav.helse.flex.testutil.SoknadBesvarer
-import no.nav.helse.flex.util.DatoUtil
-import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.shouldContain
-import org.amshove.kluent.shouldHaveSize
+import org.amshove.kluent.*
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.redis.cache.RedisCacheManager
 import java.time.LocalDate
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -24,8 +24,18 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
     @Autowired
     private lateinit var aktiveringJob: AktiveringJob
 
+    @Autowired
+    lateinit var cacheManager: RedisCacheManager
+
     private final val fnr = "12345678901"
-    private final val basisdato = LocalDate.now()
+    private final val basisdato = LocalDate.parse("2025-01-01").plusYears(1000)
+
+    @BeforeEach
+    fun setup() {
+        cacheManager.getCache("grunnbeloep-historikk")?.clear()
+        GrunnbeloepApiMockDispatcher.clearQueue()
+        flexSyketilfelleMockRestServiceServer.reset()
+    }
 
     @AfterAll
     fun hentAlleKafkaMeldinger() {
@@ -35,6 +45,17 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
     @Test
     @Order(1)
     fun `Selvstendig næringsdrivende søknad med status FREMTIDIG opprettes når vi mottar en sykmelding`() {
+        GrunnbeloepApiMockDispatcher.enqueue(
+            (basisdato.year - 5..basisdato.year).map { year ->
+                GrunnbeloepResponse(
+                    dato = "$year-05-01",
+                    grunnbeløp = 118_620 + (year - 2024) * 10_000,
+                    gjennomsnittPerÅr =
+                        118_620 + (year - 2024) * 10_000,
+                )
+            },
+        )
+
         val kafkaSoknader =
             sendSykmelding(
                 sykmeldingKafkaMessage(
@@ -42,22 +63,25 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
                     fnr = fnr,
                     sykmeldingsperioder =
                         heltSykmeldt(
-                            fom = basisdato.minusDays(1),
-                            tom = basisdato.plusDays(7),
+                            fom = basisdato,
+                            tom = basisdato.plusDays(30),
                         ) +
                             heltSykmeldt(
-                                fom = basisdato.plusDays(8),
-                                tom = basisdato.plusDays(15),
+                                fom = basisdato.plusDays(31),
+                                tom = basisdato.plusDays(60),
                             ),
                 ),
+                forventaSoknader = 2,
             )
-        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.FREMTIDIG
-        kafkaSoknader[0].arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
+        kafkaSoknader.first().status `should be equal to` SoknadsstatusDTO.FREMTIDIG
+        kafkaSoknader.first().arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
 
         val hentetViaRest = hentSoknaderMetadata(fnr)
-        hentetViaRest.shouldHaveSize(1)
+        hentetViaRest.shouldHaveSize(2)
         hentetViaRest[0].soknadstype `should be equal to` RSSoknadstype.SELVSTENDIGE_OG_FRILANSERE
         hentetViaRest[0].status `should be equal to` RSSoknadstatus.FREMTIDIG
+        hentetViaRest[1].soknadstype `should be equal to` RSSoknadstype.SELVSTENDIGE_OG_FRILANSERE
+        hentetViaRest[1].status `should be equal to` RSSoknadstatus.FREMTIDIG
     }
 
     @Test
@@ -68,18 +92,29 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
                 soknadId = hentSoknaderMetadata(fnr).first().id,
                 fnr = fnr,
             )
-        soknad.sporsmal!!.shouldHaveSize(0)
+        soknad.sporsmal.`should not be null`().shouldHaveSize(0)
     }
 
     @Test
     @Order(3)
     fun `Vi aktiverer søknaden`() {
-        aktiveringJob.bestillAktivering(now = basisdato.plusDays(16))
+        GrunnbeloepApiMockDispatcher.enqueue(
+            (basisdato.year - 5..basisdato.year).map { year ->
+                GrunnbeloepResponse(
+                    dato = "$year-05-01",
+                    grunnbeløp = 118_620 + (year - 2024) * 10_000,
+                    gjennomsnittPerÅr =
+                        118_620 + (year - 2024) * 10_000,
+                )
+            },
+        )
+
+        aktiveringJob.bestillAktivering(now = basisdato.plusDays(31))
         val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
 
         kafkaSoknader.shouldHaveSize(1)
-        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.NY
-        kafkaSoknader[0].arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
+        kafkaSoknader.first().status `should be equal to` SoknadsstatusDTO.NY
+        kafkaSoknader.first().arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
     }
 
     @Test
@@ -97,7 +132,6 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
                 FRAVAR_FOR_SYKMELDINGEN_V2,
                 TILBAKE_I_ARBEID,
                 "ARBEID_UNDERVEIS_100_PROSENT_0",
-                "ARBEID_UNDERVEIS_100_PROSENT_1",
                 ARBEID_UTENFOR_NORGE,
                 ANDRE_INNTEKTSKILDER,
                 OPPHOLD_UTENFOR_EOS,
@@ -106,18 +140,10 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
             )
 
         soknad.sporsmal.first { it.tag == "ARBEID_UNDERVEIS_100_PROSENT_0" }.sporsmalstekst!!.shouldContain(
-            DatoUtil.formatterPeriode(
-                basisdato.minusDays(1),
-                basisdato.plusDays(7),
-            ),
+            "1. - 31. januar 3025",
         )
 
-        soknad.sporsmal.first { it.tag == "ARBEID_UNDERVEIS_100_PROSENT_1" }.sporsmalstekst!!.shouldContain(
-            DatoUtil.formatterPeriode(
-                basisdato.plusDays(8),
-                basisdato.plusDays(15),
-            ),
-        )
+        soknad.forstegangssoknad.`should not be null`().`should be true`()
     }
 
     @Test
@@ -161,7 +187,45 @@ class SelvstendignaringsdrivendeFremtidigOgAktiveringTest : FellesTestOppsett() 
         val kafkaSoknader = sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
 
         kafkaSoknader.shouldHaveSize(1)
-        kafkaSoknader[0].status `should be equal to` SoknadsstatusDTO.SENDT
-        kafkaSoknader[0].arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
+        kafkaSoknader.first().status `should be equal to` SoknadsstatusDTO.SENDT
+        kafkaSoknader.first().arbeidssituasjon `should be equal to` ArbeidssituasjonDTO.SELVSTENDIG_NARINGSDRIVENDE
+    }
+
+    @Test
+    @Order(6)
+    fun `Søknad nr 2 er ikke førstegangssøknad, og inneholder riktige spørsmål`() {
+        GrunnbeloepApiMockDispatcher.enqueue(
+            (basisdato.year - 5..basisdato.year).map { year ->
+                GrunnbeloepResponse(
+                    dato = "$year-05-01",
+                    grunnbeløp = 118_620 + (year - 2024) * 10_000,
+                    gjennomsnittPerÅr =
+                        118_620 + (year - 2024) * 10_000,
+                )
+            },
+        )
+
+        aktiveringJob.bestillAktivering(now = basisdato.plusDays(61))
+        sykepengesoknadKafkaConsumer.ventPåRecords(antall = 1).tilSoknader()
+
+        val soknad =
+            hentSoknad(
+                soknadId = hentSoknaderMetadata(fnr).first { it.status == RSSoknadstatus.NY }.id,
+                fnr = fnr,
+            )
+
+        soknad.forstegangssoknad
+            .`should not be null`()
+            .`should be false`()
+
+        soknad.sporsmal!!.map { it.tag } `should be equal to`
+            listOf(
+                ANSVARSERKLARING,
+                TILBAKE_I_ARBEID,
+                "ARBEID_UNDERVEIS_100_PROSENT_0",
+                ANDRE_INNTEKTSKILDER,
+                OPPHOLD_UTENFOR_EOS,
+                TIL_SLUTT,
+            )
     }
 }
