562d082e74ddd290883a093d6f634adbc3c7e20f
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-12-11 14:16:55 +0100

    Bump json-schema-validator til 2.0.0 (#1332)

--- src/test/kotlin/no/nav/helse/flex/testutil/SubsumsjonAssertions.kt
+++ src/test/kotlin/no/nav/helse/flex/testutil/SubsumsjonAssertions.kt
@@ -2,22 +2,26 @@ package no.nav.helse.flex.testutil
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.networknt.schema.JsonSchemaFactory
-import com.networknt.schema.SpecVersion.VersionFlag.V7
-import com.networknt.schema.ValidationMessage
+import com.networknt.schema.Error
+import com.networknt.schema.InputFormat
+import com.networknt.schema.Schema
+import com.networknt.schema.SchemaRegistry
+import com.networknt.schema.SpecificationVersion
 import org.junit.jupiter.api.Assertions.assertEquals
 import java.net.URI
 
 internal object SubsumsjonAssertions {
     private val objectMapper = jacksonObjectMapper()
-    private val schema by lazy {
-        JsonSchemaFactory
-            .getInstance(V7)
-            .getSchema(URI("https://raw.githubusercontent.com/navikt/helse/main/subsumsjon/json-schema-1.0.0.json"))
+    private val schema: Schema by lazy {
+        val schemaUri = URI("https://raw.githubusercontent.com/navikt/helse/main/subsumsjon/json-schema-1.0.0.json")
+        val schemaData = schemaUri.toURL().readText()
+        val registry = SchemaRegistry.withDefaultDialect(SpecificationVersion.DRAFT_7)
+        registry.getSchema(schemaData, InputFormat.JSON)
     }
 
     internal fun assertSubsumsjonsmelding(melding: JsonNode) {
-        assertEquals(emptySet<ValidationMessage>(), schema.validate(melding))
+        val errors: List<Error> = schema.validate(melding.toString(), InputFormat.JSON)
+        assertEquals(0, errors.size)
     }
 
     internal fun assertSubsumsjonsmelding(melding: String) = assertSubsumsjonsmelding(objectMapper.readTree(melding))
