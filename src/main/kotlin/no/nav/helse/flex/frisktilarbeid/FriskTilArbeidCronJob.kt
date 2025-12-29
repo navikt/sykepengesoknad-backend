a8a26e542f79cbb5715f46163296006704c6fcd6
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2025-12-29 15:49:28 +0100

    Revert reset av FriskTilArbeid fom og tom (#1346)
    * Revert "Rett feil på grunn av opprom (space) i ID (#1345)"
    
    This reverts commit b203670f41ba20c3c18577ced9e180a9e3a6d003.
    
    * Revert "Reset FriskTilArbeid vedtak fom og tom (#1344)"
    
    This reverts commit b23242b0682f6dd84777826d86aa9f8ff3b9a904.

--- src/main/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidCronJob.kt
+++ src/main/kotlin/no/nav/helse/flex/frisktilarbeid/FriskTilArbeidCronJob.kt
@@ -16,24 +16,8 @@ class FriskTilArbeidCronJob(
     private val leaderElection: LeaderElection,
     private val friskTilArbeidService: FriskTilArbeidService,
     private val aktiveringProducer: AktiveringProducer,
-    private val friskTilArbeidRepository: FriskTilArbeidRepository,
 ) {
     private val log = logger()
-
-    @Scheduled(initialDelay = 4, fixedDelay = 3600, timeUnit = TimeUnit.MINUTES)
-    fun resetVedtaksperiodeFomTom() {
-        if (leaderElection.isLeader()) {
-            val t1 = Triple("a76f6e88-4701-45fb-ab63-80874c15cf4f", LocalDate.of(2025, 11, 29), LocalDate.of(2026, 2, 21))
-            val t2 = Triple("56fac704-37e0-4906-bb8b-566398604d56", LocalDate.of(2025, 12, 4), LocalDate.of(2026, 2, 26))
-
-            listOf(t1, t2).forEach { tripple ->
-                friskTilArbeidRepository.findById(tripple.first).get().also {
-                    friskTilArbeidRepository.save(it.copy(fom = tripple.second, tom = tripple.third))
-                    log.info("Endret FriskTilArbeid vedtak: ${it.id} til fom: ${tripple.second} og tom: ${tripple.third}")
-                }
-            }
-        }
-    }
 
     @Scheduled(initialDelay = 5, fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
     fun schedulertStartBehandlingAvFriskTilArbeidVedtakStatus() {
