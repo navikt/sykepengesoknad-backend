e5ab93b3b53091133c8dc9d69b0ad7e1e712f557
Ole Bastian Løchen <42940656+olebaba@users.noreply.github.com>
2026-02-13 09:44:19 +0100

    BrregClient håndterer klient-feil ved å returnere tom liste (#1375)

--- src/main/kotlin/no/nav/helse/flex/client/brreg/BrregClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/brreg/BrregClient.kt
@@ -1,8 +1,10 @@
 package no.nav.helse.flex.client.brreg
 
+import no.nav.helse.flex.logger
 import org.springframework.http.MediaType
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.toEntity
@@ -11,21 +13,28 @@ import org.springframework.web.client.toEntity
 class BrregClient(
     private val brregRestClient: RestClient,
 ) {
+    val log = logger()
+
     @Retryable(include = [HttpServerErrorException::class])
     fun hentRoller(
         fnr: String,
         rolleTyper: List<Rolletype>? = null,
     ): List<RolleDto> {
-        val uri = brregRestClient.post().uri { uriBuilder -> uriBuilder.path("/api/v1/roller").build() }
-        val hentRollerRequest = HentRollerRequest(fnr = fnr, rolleTyper = rolleTyper)
+        try {
+            val uri = brregRestClient.post().uri { uriBuilder -> uriBuilder.path("/api/v1/roller").build() }
+            val hentRollerRequest = HentRollerRequest(fnr = fnr, rolleTyper = rolleTyper)
 
-        return uri
-            .headers {
-                it.contentType = MediaType.APPLICATION_JSON
-            }.body(hentRollerRequest)
-            .retrieve()
-            .toEntity<RollerDto>()
-            .body
-            ?.roller ?: emptyList()
+            return uri
+                .headers {
+                    it.contentType = MediaType.APPLICATION_JSON
+                }.body(hentRollerRequest)
+                .retrieve()
+                .toEntity<RollerDto>()
+                .body
+                ?.roller ?: emptyList()
+        } catch (e: HttpClientErrorException) {
+            log.error("Brreg returnerte ${e.statusCode}. Returnerer tom liste.")
+            return emptyList()
+        }
     }
 }
