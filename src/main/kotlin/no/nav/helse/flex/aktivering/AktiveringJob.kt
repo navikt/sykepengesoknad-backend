d1dd07c7cb8d1b1750746bb22fc39e7af4ee3154
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2026-03-05 13:25:36 +0100

    Kjør aktivering hver time (#1392)
    Hver 5. minutt gjør at samme søknad forsøkes aktiveres flere ganger.

--- src/main/kotlin/no/nav/helse/flex/aktivering/AktiveringJob.kt
+++ src/main/kotlin/no/nav/helse/flex/aktivering/AktiveringJob.kt
@@ -18,7 +18,7 @@ class AktiveringJob(
 ) {
     private val log = logger()
 
-    @Scheduled(initialDelay = 5, fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+    @Scheduled(initialDelay = 5, fixedDelay = 60, timeUnit = TimeUnit.MINUTES)
     fun startBestillAktivering() {
         log.info("Kjører scheduled jobb startBestillAktivering.")
         if (leaderElection.isLeader()) {
