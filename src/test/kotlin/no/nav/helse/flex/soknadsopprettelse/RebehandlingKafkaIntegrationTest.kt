2beac2219ea0563dd1cd6c5efb8e69e351a39555
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-04-21 10:55:31 +0200

    Flytter tester for rebehandling til egen fil

---
+++ src/test/kotlin/no/nav/helse/flex/soknadsopprettelse/RebehandlingKafkaIntegrationTest.kt
@@ -0,0 +1,130 @@
+package no.nav.helse.flex.soknadsopprettelse
+
+import com.nhaarman.mockitokotlin2.any
+import no.nav.helse.flex.FellesTestOppsett
+import no.nav.helse.flex.domain.Arbeidssituasjon
+import no.nav.helse.flex.kafka.consumer.SYKMELDINGSENDT_TOPIC
+import no.nav.helse.flex.mockFlexSyketilfelleErUtenforVentetid
+import no.nav.helse.flex.mockFlexSyketilfelleHentSykmeldingerMedSammeVentetidDefault
+import no.nav.helse.flex.mockFlexSyketilfelleSykeforloep
+import no.nav.helse.flex.testdata.skapArbeidsgiverSykmelding
+import no.nav.helse.flex.testdata.skapSykmeldingStatusKafkaMessageDTO
+import no.nav.syfo.model.sykmelding.arbeidsgiver.SykmeldingsperiodeAGDTO
+import no.nav.syfo.model.sykmelding.model.PeriodetypeDTO
+import no.nav.syfo.sykmelding.kafka.model.ArbeidsgiverStatusKafkaDTO
+import no.nav.syfo.sykmelding.kafka.model.STATUS_SENDT
+import no.nav.syfo.sykmelding.kafka.model.SykmeldingKafkaMessageDTO
+import no.nav.syfo.sykmelding.kafka.model.SykmeldingStatusKafkaMessageDTO
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import java.time.LocalDate
+
+class RebehandlingKafkaIntegrationTest : FellesTestOppsett() {
+    private val fnr = "123456789"
+    private val dato = LocalDate.of(2025, 1, 1)
+
+    @BeforeEach
+    fun setUp() {
+        databaseReset.resetDatabase()
+        flexSyketilfelleMockRestServiceServer.reset()
+        fakeUnleash.resetAll()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        databaseReset.resetDatabase()
+    }
+
+    @Test
+    fun `Legger til rebehandling UventetArbeidssituasjonException`() {
+        val sykmeldingStatusKafkaMessageDTO = skapKafkaMelding(arbeidssituasjon = Arbeidssituasjon.FRILANSER)
+        val sykmelding = skapSykmeldingDTO(sykmeldingStatusKafkaMessageDTO)
+
+        val sykmeldingKafkaMessage =
+            SykmeldingKafkaMessageDTO(
+                sykmelding = sykmelding,
+                event = sykmeldingStatusKafkaMessageDTO.event,
+                kafkaMetadata = sykmeldingStatusKafkaMessageDTO.kafkaMetadata,
+            )
+
+        behandleSykmeldingOgBestillAktivering.prosesserSykmelding(
+            sykmelding.id,
+            sykmeldingKafkaMessage,
+            SYKMELDINGSENDT_TOPIC,
+        )
+        verifiserRebehandling()
+    }
+
+    @Test
+    fun `Legger til rebehandling ManglerArbeidsgiverException`() {
+        val sykmeldingStatusKafkaMessageDTO = skapKafkaMelding(arbeidsgiver = null)
+        val sykmelding = skapSykmeldingDTO(sykmeldingStatusKafkaMessageDTO)
+
+        val sykmeldingKafkaMessage =
+            SykmeldingKafkaMessageDTO(
+                sykmelding = sykmelding,
+                event = sykmeldingStatusKafkaMessageDTO.event,
+                kafkaMetadata = sykmeldingStatusKafkaMessageDTO.kafkaMetadata,
+            )
+        mockStandardSyketilfelle(sykmelding.id)
+
+        behandleSykmeldingOgBestillAktivering.prosesserSykmelding(
+            sykmelding.id,
+            sykmeldingKafkaMessage,
+            SYKMELDINGSENDT_TOPIC,
+        )
+        verifiserRebehandling()
+    }
+
+    private fun verifiserRebehandling() {
+        Mockito.verify(rebehandlingsSykmeldingSendtProducer, Mockito.times(1)).leggPaRebehandlingTopic(any(), any())
+    }
+
+    private fun mockStandardSyketilfelle(
+        sykmeldingId: String,
+        erUtenforVentetid: Boolean? = null,
+        oppfolgingsdato: LocalDate = dato,
+    ) {
+        erUtenforVentetid?.let {
+            mockFlexSyketilfelleErUtenforVentetid(sykmeldingId, it)
+        }
+        mockFlexSyketilfelleSykeforloep(sykmeldingId, oppfolgingsdato)
+        mockFlexSyketilfelleHentSykmeldingerMedSammeVentetidDefault(sykmeldingId)
+    }
+
+    private fun skapKafkaMelding(
+        statusEvent: String = STATUS_SENDT,
+        arbeidssituasjon: Arbeidssituasjon = Arbeidssituasjon.ARBEIDSTAKER,
+        arbeidsgiver: ArbeidsgiverStatusKafkaDTO? = null,
+    ) = skapSykmeldingStatusKafkaMessageDTO(
+        fnr = fnr,
+        statusEvent = statusEvent,
+        arbeidssituasjon = arbeidssituasjon,
+        arbeidsgiver = arbeidsgiver,
+    )
+
+    private fun skapSykmeldingDTO(
+        sykmeldingStatusKafkaMessageDTO: SykmeldingStatusKafkaMessageDTO,
+        syketilfelleStartDato: LocalDate? = LocalDate.of(2020, 2, 1),
+        sykmeldingsperioder: List<SykmeldingsperiodeAGDTO> =
+            listOf(
+                SykmeldingsperiodeAGDTO(
+                    fom = LocalDate.of(2020, 2, 1),
+                    tom = LocalDate.of(2020, 2, 5),
+                    type = PeriodetypeDTO.AKTIVITET_IKKE_MULIG,
+                    reisetilskudd = false,
+                    aktivitetIkkeMulig = null,
+                    behandlingsdager = null,
+                    gradert = null,
+                    innspillTilArbeidsgiver = null,
+                ),
+            ),
+    ) = skapArbeidsgiverSykmelding(
+        sykmeldingId = sykmeldingStatusKafkaMessageDTO.event.sykmeldingId,
+    ).copy(
+        sykmeldingsperioder = sykmeldingsperioder,
+        syketilfelleStartDato = syketilfelleStartDato,
+    )
+}
