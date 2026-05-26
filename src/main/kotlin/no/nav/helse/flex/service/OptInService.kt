d7456f9607dab9de9d093f9aac5067a8577daffd
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-05-26 09:01:29 +0200

    Trekker ut logikk for opprettelse av opt in til egen service

---
+++ src/main/kotlin/no/nav/helse/flex/service/OptInService.kt
@@ -0,0 +1,47 @@
+package no.nav.helse.flex.service
+
+import no.nav.helse.flex.domain.Arbeidssituasjon
+import no.nav.helse.flex.exception.UgyldigOptInSykmeldingException
+import no.nav.helse.flex.logger
+import no.nav.helse.flex.soknadsopprettelse.BehandleSykmeldingOgBestillAktivering
+import no.nav.helse.flex.soknadsopprettelse.hentArbeidssituasjon
+import no.nav.syfo.sykmelding.kafka.model.STATUS_BEKREFTET
+import no.nav.syfo.sykmelding.kafka.model.SykmeldingKafkaMessageDTO
+import org.springframework.stereotype.Service
+import java.time.OffsetDateTime
+
+@Service
+class OptInService(
+    private val behandleSykmeldingOgBestillAktivering: BehandleSykmeldingOgBestillAktivering,
+) {
+    private val log = logger()
+
+    fun opprettOptInnSoknad(sykmeldingKafkaMessage: SykmeldingKafkaMessageDTO) {
+        val arbeidssituasjon = sykmeldingKafkaMessage.validerOgHentOptInArbeidssituasjon()
+        if (sykmeldingKafkaMessage.timestampInnenfor4Mnd().not()) {
+            throw UgyldigOptInSykmeldingException("Sykmelding ${sykmeldingKafkaMessage.sykmelding.id} er for gammel for opt-in")
+        }
+
+        behandleSykmeldingOgBestillAktivering.prosesserSykmeldingMedOptIn(sykmeldingKafkaMessage)
+        log.info("Prosesserte opt-in sykmelding ${sykmeldingKafkaMessage.sykmelding.id} for arbeidssituasjon $arbeidssituasjon")
+    }
+
+    private fun SykmeldingKafkaMessageDTO.validerOgHentOptInArbeidssituasjon(): Arbeidssituasjon {
+        if (event.statusEvent != STATUS_BEKREFTET) {
+            throw UgyldigOptInSykmeldingException(
+                "Sykmelding ${sykmelding.id} har ugyldig statusEvent ${event.statusEvent}, forventet $STATUS_BEKREFTET",
+            )
+        }
+        val arbeidssituasjon =
+            hentArbeidssituasjon()
+                ?: throw UgyldigOptInSykmeldingException("Fant ikke arbeidssituasjon for sykmelding ${sykmelding.id}")
+        if (arbeidssituasjon !in setOf(Arbeidssituasjon.FRILANSER, Arbeidssituasjon.NAERINGSDRIVENDE)) {
+            throw UgyldigOptInSykmeldingException(
+                "Ugyldig arbeidssituasjon $arbeidssituasjon for sykmelding ${sykmelding.id}",
+            )
+        }
+        return arbeidssituasjon
+    }
+
+    private fun SykmeldingKafkaMessageDTO.timestampInnenfor4Mnd(): Boolean = event.timestamp.isAfter(OffsetDateTime.now().minusMonths(4))
+}
