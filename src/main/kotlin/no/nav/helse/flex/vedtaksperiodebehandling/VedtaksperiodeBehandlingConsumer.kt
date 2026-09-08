3b204b1880df5637e6e9c06e230b6bab77f8c5ce
Steinar Sørli Hegna <57735098+steinshe@users.noreply.github.com>
2026-09-08 18:43:07 +0200

    Hold offset up to date imens reprosessering kjører (#1563)

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
@@ -1,28 +1,29 @@
 package no.nav.helse.flex.vedtaksperiodebehandling
 
-import com.fasterxml.jackson.module.kotlin.readValue
+import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.helse.flex.kafka.SIS_TOPIC
 import no.nav.helse.flex.logger
-import no.nav.helse.flex.util.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
-class VedtaksperiodeBehandlingConsumer(
-    private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
-) {
+class VedtaksperiodeBehandlingConsumer {
     val log = logger()
 
+    @WithSpan
+    @KafkaListener(
+        topics = [SIS_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        id = "sis-consumer",
+        idIsGroup = false,
+    )
     fun listen(
         cr: ConsumerRecord<String, String>,
         acknowledgment: Acknowledgment,
     ) {
-        val meldingMetadata: MeldingMetadata = objectMapper.readValue(cr.value())
-
-        if (meldingMetadata.eventName == "behandlingstatus") {
-            val kafkaDto: Behandlingstatusmelding = objectMapper.readValue(cr.value())
-            prosseserKafkaMeldingFraSpleiselaget.prosesserKafkaMelding(kafkaDto)
-        }
+        log.info("Holder offset up to date imens reprosessering kjører")
 
         acknowledgment.acknowledge()
     }
