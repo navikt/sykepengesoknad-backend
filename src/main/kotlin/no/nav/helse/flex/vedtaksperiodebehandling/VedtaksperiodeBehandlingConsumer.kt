9feb03e36047802b65220455ffee588c4a74e506
Steinar Sørli Hegna <57735098+steinshe@users.noreply.github.com>
2026-09-09 13:22:11 +0200

    Hold offset up to date imens reprosessering kjører (#1565)

--- src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/vedtaksperiodebehandling/VedtaksperiodeBehandlingConsumer.kt
@@ -1,18 +1,30 @@
 package no.nav.helse.flex.vedtaksperiodebehandling
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import io.opentelemetry.instrumentation.annotations.WithSpan
+import no.nav.helse.flex.kafka.SIS_TOPIC
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.util.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Profile
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
+@Profile("test")
 @Component
 class VedtaksperiodeBehandlingConsumer(
     private val prosseserKafkaMeldingFraSpleiselaget: ProsseserKafkaMeldingFraSpleiselaget,
 ) {
     val log = logger()
 
+    @WithSpan
+    @KafkaListener(
+        topics = [SIS_TOPIC],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        id = "sis-consumer",
+        idIsGroup = false,
+    )
     fun listen(
         cr: ConsumerRecord<String, String>,
         acknowledgment: Acknowledgment,
